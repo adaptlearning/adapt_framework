5a3d3e2b140ba803570afcffb2efdd4cb68eb1e8
Fabien O'Carroll <fabien@Fabiens-Mac-mini.local>
2013-10-25 16:38:56 +0100

    Adding core folder
    * Contains adapt.js

---
+++ src/core/js/adapt.js
@@ -0,0 +1,14 @@
+/*
+* Adapt
+* License - http://github.com/adaptlearning/adapt_framework/LICENSE
+* Author - Fabien O'Carroll
+* Maintainers - Fabien O'Carroll
+*/
+
+define(function(){
+    var Adapt = {};
+
+    _.extend(Adapt, Backbone.Events);
+
+    return Adapt;
+});
