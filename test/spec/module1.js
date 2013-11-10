377c3fb4a391bb76421bcd6626ada9d8f0e6061d
Daryl Hedley <darylhedley@hotmail.com>
2013-11-10 12:14:33 +0000

    Updated to include a second test

--- test/spec/module1.js
+++ test/spec/module1.js
@@ -1,13 +1,15 @@
-// load the entire module/library and pass to the test
 define(['coreJS/adapt'],function(Adapt) {
 
-  // use jasmine to run tests against the required code
-  describe('Adapt', function() {
-
-    it('should be faking a test of true', function() {
-      expect(true).toBe(true);
+    describe('Adapt', function() {
+        it('should be faking a test of true', function() {
+            expect(true).toBe(true);
+        });
     });
-
-  });
+    
+    describe('Adapt', function() {
+        it('should be faking a test of false', function() {
+            expect(false).toBe(false);
+        });
+    });
 
 });
--- test/test-main.js
+++ test/test-main.js
@@ -1,32 +1,30 @@
 var tests = [];
 for (var file in window.__karma__.files) {
-  if (/spec\//.test(file)) {
-    tests.push(file);
-  }
+    if (/spec\//.test(file)) {
+        tests.push(file);
+    }
 }
 
 requirejs.config({
-  baseUrl: '/base/src',
-  paths: {
-    jquery: 'core/js/libraries/jquery',
-    underscore: 'core/js/libraries/underscore',
-    backbone: 'core/js/libraries/backbone',
-    modernizr: 'core/js/libraries/modernizr',
-    handlebars: 'core/js/libraries/handlebars',
-    imageReady: 'core/js/libraries/imageReady',
-    inview: 'core/js/libraries/inview',
-    scrollTo: 'core/js/libraries/scrollTo',
-    coreJS: 'core/js',
-    coreViews: 'core/js/views',
-    coreModels: 'core/js/models',
-    coreCollections: 'core/js/collections',
-    templates: 'templates/templates',
-    spec: '../test/spec'
-  },
+    baseUrl: '/base/src',
+    paths: {
+        jquery: 'core/js/libraries/jquery',
+        underscore: 'core/js/libraries/underscore',
+        backbone: 'core/js/libraries/backbone',
+        modernizr: 'core/js/libraries/modernizr',
+        handlebars: 'core/js/libraries/handlebars',
+        imageReady: 'core/js/libraries/imageReady',
+        inview: 'core/js/libraries/inview',
+        scrollTo: 'core/js/libraries/scrollTo',
+        coreJS: 'core/js',
+        coreViews: 'core/js/views',
+        coreModels: 'core/js/models',
+        coreCollections: 'core/js/collections',
+        templates: 'templates/templates',
+        spec: '../test/spec'
+    },
     shim: {
-        jquery: [
-    
-        ],
+        jquery: [],
         backbone: {
             deps: [
                 'underscore',
@@ -41,6 +39,6 @@ requirejs.config({
             exports: 'Handlebars'
         }
     },
-  deps: tests,
-  callback: window.__karma__.start
+    deps: tests,
+    callback: window.__karma__.start
 });
