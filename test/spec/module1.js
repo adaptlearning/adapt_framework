9792b9650ce5b85f1681c5e673aa4e505e60f0fc
Daryl Hedley <darylhedley@hotmail.com>
2013-11-10 10:23:35 +0000

    Updated tests

--- test/spec/module1.js
+++ test/spec/module1.js
@@ -1,11 +1,11 @@
 // load the entire module/library and pass to the test
-define(['mylib'],function(mylib) {
+define(['coreJS/adapt'],function(Adapt) {
 
   // use jasmine to run tests against the required code
-  describe('feature', function() {
+  describe('Adapt', function() {
 
-    it('should be working ;)', function() {
-      expect(mylib.feature).toBe('Custom feature');
+    it('should be faking a test of true', function() {
+      expect(true).toBe(true);
     });
 
   });
--- test/test-main.js
+++ test/test-main.js
@@ -7,40 +7,7 @@ for (var file in window.__karma__.files) {
 
 require.config({
     baseUrl: "src",
-    /*mainConfigFile: "./config.js",*/
-    paths: {
-        jquery: 'core/js/libraries/jquery',
-        underscore: 'core/js/libraries/underscore',
-        backbone: 'core/js/libraries/backbone',
-        modernizr: 'core/js/libraries/modernizr',
-        handlebars: 'core/js/libraries/handlebars',
-        imageReady: 'core/js/libraries/imageReady',
-        inview: 'core/js/libraries/inview',
-        scrollTo: 'core/js/libraries/scrollTo',
-        coreJS: 'core/js',
-        coreViews: 'core/js/views',
-        coreModels: 'core/js/models',
-        coreCollections: 'core/js/collections',
-        templates: 'templates/templates'
-    },
-    deps: tests,
-    shim: {
-        jquery: [
-
-        ],
-        backbone: {
-            deps: [
-                'underscore',
-                'jquery'
-            ],
-            exports: 'Backbone'
-        },
-        underscore: {
-            exports: '_'
-        },
-        handlebars: {
-            exports: 'Handlebars'
-        }
-    },
+    mainConfigFile: "./config.js",
+    deps: tests
     callback: window.__karma__.start
 });
