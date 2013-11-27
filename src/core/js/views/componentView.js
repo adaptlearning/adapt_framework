0ce001f16b03b4c4b9a3be59e7aebe4a196be5c7
Daryl Hedley <darylhedley@hotmail.com>
2013-11-27 14:11:56 +0000

    Added feature/8281
    - Updated .css files to .less files
    - Added _layout attribute to components

--- src/core/js/views/componentView.js
+++ src/core/js/views/componentView.js
@@ -9,7 +9,10 @@ define(["handlebars", "coreViews/adaptView"], function(Handlebars, AdaptView) {
     var ComponentView = AdaptView.extend({
     
         className: function() {
-            return "component " + this.model.get('_component')+"-component " + this.model.get('_id');
+            return "component " 
+            + this.model.get('_component')
+            + "-component " + this.model.get('_id') 
+            + " component-" + this.model.get('_layout');
         },
         
         init: function() {
