37c1739977ba35f6333fd6775fc5e3ad4ad8989b
Daryl Hedley <darylhedley@hotmail.com>
2014-01-07 14:10:55 +0000

    Fixed bug with menuView.js

--- src/core/js/views/menuView.js
+++ src/core/js/views/menuView.js
@@ -16,8 +16,21 @@ define(function(require) {
             + 'menu-' 
             + this.model.get('_id');
     	},
+
+        preRender: function() {
+            this.$el.css('opacity', 0);
+            this.listenTo(this.model, 'change:_isReady', this.isReady);
+        }
         
         postRender: function() { 
+        },
+        
+        isReady: function() {
+            _.defer(_.bind(function() {
+                $('.loading').hide();
+                this.$el.animate({'opacity': 1}, 'fast');
+                Adapt.trigger('menuView:ready');
+            }, this));
         }
         
     });
