7df685b9f84f442e9809f4ef3d08cc9947b72b23
Daryl Hedley <darylhedley@hotmail.com>
2014-01-23 11:32:34 +0000

    Updated scriptLoader

--- src/core/js/scriptLoader.js
+++ src/core/js/scriptLoader.js
@@ -7,15 +7,15 @@
 Modernizr.load([
     {
         test: window.JSON,
-        nope: 'core/js/libraries/json2.js'
+        nope: 'libraries/json2.js'
     },
     {
-        test: navigator.userAgent.match(/MSIE\s(?!9.0)/),
-        nope: 'core/js/libraries/consoles.js'
+        test: document.querySelector,
+        nope: 'libraries/consoles.js'
     },
     {
         test: Modernizr.video || Modernizr.audio,
-        nope: 'core/js/libraries/swfObject.js',
+        nope: 'libraries/swfObject.js',
         complete: function() {
             yepnope.injectJs("libraries/require.js", function () {   
             }, {
