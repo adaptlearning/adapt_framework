ac8cb7648f8858472074624979c1821c3b6e4fd1
Chris Jones <chris.jones@spongeuk.com>
2014-02-03 15:52:49 +0000

    block view triggers window scroll on visiblity change to make inview fire

--- src/core/js/views/blockView.js
+++ src/core/js/views/blockView.js
@@ -11,12 +11,23 @@ define(function(require) {
     var BlockView = AdaptView.extend({
         
         className: function() {
-            return "block " 
-            + this.model.get('_id') 
-            + " " + this.model.get('_classes')
-            + " " + this.setVisibility() 
-            + " nth-child-" 
-            + this.options.nthChild;;
+            return [
+                "block ",
+                this.model.get('_id'),
+                this.model.get('_classes'),
+                this.setVisibility(),
+                "nth-child-" + this.options.nthChild
+            ].join(' ');
+        },
+
+        preRender: function() {
+            this.listenTo(this.model, 'change:_isVisible', this.isVisible);
+        },
+        
+        isVisible: function() {
+            _.defer(_.bind(function() {
+                $(window).scroll();
+            }, this));
         }
         
     }, {
