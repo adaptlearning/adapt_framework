fd9a5a30331e0394ecd6918924a83ddb98a3e131
Daryl Hedley <darylhedley@hotmail.com>
2014-02-11 13:48:49 +0000

    Updated to spaces for Brian

--- src/core/js/popupManager.js
+++ src/core/js/popupManager.js
@@ -1,15 +1,14 @@
 define(function(require) {
 
-	var Adapt = require('coreJS/adapt');
+    var Adapt = require('coreJS/adapt');
+    var scrollTop = 0;
 
-	var scrollTop = 0;
+    Adapt.on('popup:opened', function() {
+        scrollTop = $(window).scrollTop();
+    });
 
-	Adapt.on('popup:opened', function() {
-		scrollTop = $(window).scrollTop();
-	});
-
-	Adapt.on('popup:closed', function() {
-		$(window).scrollTop(scrollTop);
-	})
+    Adapt.on('popup:closed', function() {
+        $(window).scrollTop(scrollTop);
+    });
 
 });
