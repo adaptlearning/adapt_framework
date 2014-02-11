9debf7025af02ac06fa0043fbafe9a71a7da150c
Daryl Hedley <darylhedley@hotmail.com>
2014-02-11 15:54:58 +0000

    Initial commit of notify

---
+++ src/core/js/notify.js
@@ -0,0 +1,13 @@
+/*
+* Notify
+* License - http://github.com/adaptlearning/adapt_framework/LICENSE
+* Maintainers - Daryl Hedley <darylhedley@hotmail.com>
+*/
+
+define(function(require) {
+
+	var Notify = (){};
+
+	console.log(Notify);
+
+});
