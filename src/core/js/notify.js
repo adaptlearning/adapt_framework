2f5596e89be425debcf6e0a2d9a1d25364d675da
Daryl Hedley <darylhedley@hotmail.com>
2014-04-25 17:40:44 +0100

    Added push notifications

--- src/core/js/notify.js
+++ src/core/js/notify.js
@@ -9,6 +9,53 @@ define(function(require) {
 	var Adapt = require('coreJS/adapt');
 	var NotifyView = require('coreViews/notifyView');
 	var NotifyPushView = require('coreViews/notifyPushView');
+	var NotifyModel = require('coreModels/notifyModel');
+
+	// Build a collection to store push notifications
+	var NotifyPushCollection = Backbone.Collection.extend({
+
+		model: NotifyModel,
+
+		initialize: function() {
+			this.listenTo(this, 'add', this.onPushAdded);
+			this.listenTo(Adapt, 'notify:pushRemoved', this.onRemovePush);
+		},
+
+		onPushAdded: function(model) {
+			this.checkPushCanShow(model);
+		},
+
+		checkPushCanShow: function(model) {
+			if (this.canShowPush()) {
+				model.set('_isActive', true);
+				this.showPush(model);
+			}
+		},
+
+		canShowPush: function() {
+			var availablePushNotifications = this.where({_isActive:true});
+			if (availablePushNotifications.length >= 2) {
+				return false;
+			}
+			return true;
+		},
+
+		showPush: function(model) {
+			new NotifyPushView({
+				model: model
+			});
+		},
+
+		onRemovePush: function(view) {
+			var inactivePushNotifications = this.where({_isActive:false});
+			if (inactivePushNotifications.length > 0) {
+				this.checkPushCanShow(inactivePushNotifications[0]);
+			}
+		}
+
+	});
+
+	NotifyPushes = new NotifyPushCollection();
 
 	Adapt.on('notify:alert', function(notifyObject) {
 		addNotifyView('alert', notifyObject);
@@ -31,16 +78,14 @@ define(function(require) {
 
 		if (type === 'push') {
 
-			new NotifyPushView({
-				model: new Backbone.Model(notifyObject)
-			});
+			NotifyPushes.push(notifyObject);
 
 			return;
 
 		}
 
 		new NotifyView({
-			model: new Backbone.Model(notifyObject)
+			model: new NotifyModel(notifyObject)
 		});
 	};
 
