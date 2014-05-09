60412f272311e48929e4424c52c4ebf2575231de
Dan Ghost <daniel.ghost@kineo.com>
2014-05-09 17:21:09 +0100

    Find model by ID
    Added method to find a model within any one of the four collections
    based on the ID.

--- src/core/js/models/adaptModel.js
+++ src/core/js/models/adaptModel.js
@@ -141,6 +141,23 @@ define(function(require) {
             // returns a collection of siblings
             return siblingsCollection;
         },
+
+        findByID: function (id) {
+            var collections = id.split('_');
+            var collectionID = collections[collections.length - 1]
+            var collectionType = collectionID.replace(/[0-9]+$/, '');
+
+            switch(collectionType) {
+                case "co-":
+                    return Adapt.contentObjects.findWhere({_id: collectionID});
+                case "a-":
+                    return Adapt.articles.findWhere({_id: collectionID});
+                case "b-":
+                    return Adapt.blocks.findWhere({_id: collectionID});
+                case "c-":
+                    return Adapt.components.findWhere({_id: collectionID});
+            }
+        },
         
         setOnChildren: function(key, value, options) {
             
