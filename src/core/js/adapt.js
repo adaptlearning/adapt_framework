031b99fec1cc5153e00257d403e0ceac56ee259b
kineogavin <gavin.mcmaster@kineo.com>
2014-05-21 11:07:35 +0100

    Added data for search and updated adapt.js to al v1.1

--- src/core/js/adapt.js
+++ src/core/js/adapt.js
@@ -11,6 +11,8 @@ define(function(require){
     var Helpers = require('coreHelpers');
     
     var Adapt = {};
+    Adapt.location = {};
+    Adapt.componentStore = {};
 
     _.extend(Adapt, Backbone.Events);
     
@@ -19,27 +21,55 @@ define(function(require){
         Adapt.trigger('adapt:initialize');
     });
 
-    Adapt.scrollTo = function(element, duration, settings) {
+    Adapt.scrollTo = function(selector, settings) {
         // Get the current location - this is set in the router
-        var location = $('#wrapper').attr('data-location');
+        var location = (Adapt.location._contentType) ? 
+            Adapt.location._contentType : Adapt.location.currentLocation;
         // Trigger initial scrollTo event
-        Adapt.trigger(location+':scrollTo', element);
+        Adapt.trigger(location+':scrollTo', selector);
         //Setup duration variable passed upon arguments
-        if (!settings) {
-            settings = duration;
-            duration = (settings.duration) ? settings.duration : $.scrollTo.defaults.duration;
+        var settings = (settings || {});
+        if (!settings.duration) {
+            settings.duration = $.scrollTo.defaults.duration;
         }
         // Trigger scrollTo plugin
-        $.scrollTo(element, duration, settings);
+        $.scrollTo(selector, settings);
         // Trigger an event after animation
         // 300 milliseconds added to make sure queue has finished
         _.delay(function() {
-            Adapt.trigger(location+':scrolledTo', element);
-        }, duration+300);
+            Adapt.trigger(location+':scrolledTo', selector);
+        }, settings.duration+300);
         
     }
-    
-    Adapt.componentStore = {};
+
+    Adapt.navigateToElement = function(selector, type, settings) {
+        // Allows a selector to be passed in and Adapt will navigate to this element
+
+        // Setup settings object
+        var settings = (settings || {});
+
+        // Removes . symbol from the selector to find the model
+        var currentModelId = selector.replace(/\./g, '');
+        var currentModel = Adapt[type].findWhere({_id: currentModelId});
+        // Get current page to check whether this is the current page
+        var currentPage = currentModel.findAncestor('contentObjects');
+
+        // If current page - scrollTo element
+        if (currentPage.get('_id') === Adapt.location._currentId) {
+           return Adapt.scrollTo(selector, settings);
+        }
+
+        // If the element is on another page navigate and wait until pageView:ready is fired
+        // Then scrollTo element
+        Adapt.once('pageView:ready', function() {
+            _.defer(function() {
+                Adapt.scrollTo(selector, settings)
+            })
+        });
+
+        Backbone.history.navigate('#/id/' + currentPage.get('_id'), {trigger: true});
+
+    }
     
     Adapt.register = function(name, object) {
         
