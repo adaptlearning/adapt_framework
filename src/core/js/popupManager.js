635a8b722f8b4e322dd068e202d301805387b859
Kirsty Hames <kirstyhames@Kirstys-Mac-Mini.local>
2014-06-04 17:50:42 +0100

    enable tab index to be managed by popupManager

--- src/core/js/popupManager.js
+++ src/core/js/popupManager.js
@@ -2,13 +2,20 @@ define(function(require) {
 
     var Adapt = require('coreJS/adapt');
     var scrollTop = 0;
+    var $activeElement;
 
     Adapt.on('popup:opened', function() {
-        scrollTop = $(window).scrollTop();
+    	scrollTop = $(window).scrollTop();
+    	$activeElement = $(document.activeElement);
+        $('a, button, input').attr('tabindex', -1);
     });
 
     Adapt.on('popup:closed', function() {
         $(window).scrollTop(scrollTop);
+        $('a, button, input').attr('tabindex', 0);
+        if ($activeElement) {
+        	$activeElement.focus();
+    	}
     });
 
 });
