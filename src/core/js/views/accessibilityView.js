469fa00af516aa358cf118dd4448f74856f9fbfc
Kirsty Hames <kirstyhames@Kirstys-Mac-Mini.local>
2014-06-17 11:46:07 +0100

    accessibility toggle button added

---
+++ src/core/js/views/accessibilityView.js
@@ -0,0 +1,57 @@
+/*
+* Accessibility View
+* License - https://github.com/adaptlearning/adapt_framework/blob/master/LICENSE
+* Maintainers - Kirsty Hames, Daryl Hedley
+*/
+
+define(function(require) {
+
+    var Backbone = require('backbone');
+    var Adapt = require('coreJS/adapt');
+
+    var AccessibilityView = Backbone.View.extend({
+
+        el: '.accessibility-toggle',
+
+        initialize: function() {
+            this.render();
+        },
+
+        events: {
+            'click' : 'toggleAccessibility'
+        },
+
+        render: function() {
+
+            var hasAccessibility = Adapt.config.get('_accessibility')._isEnabled;
+            var accessibilityOff = Adapt.course.get('_accessibility')._accessibilityToggleTextOff;
+            var accessibilityOn = Adapt.course.get('_accessibility')._accessibilityToggleTextOn;
+
+            var toggleText = (hasAccessibility) ? accessibilityOff : accessibilityOn;
+
+            this.$el.html(toggleText).attr('aria-label', Adapt.course.get('_accessibility')._ariaLabels.accessibilityToggleButton);
+
+        },
+
+
+
+        toggleAccessibility: function(event) {
+            event.preventDefault();
+
+            var hasAccessibility = Adapt.config.get('_accessibility')._isEnabled;
+
+            var toggleAccessibility = (hasAccessibility) ? false : true;
+
+            Adapt.config.get('_accessibility')._isEnabled = toggleAccessibility;
+
+            Adapt.trigger('accessibility:toggle');
+
+            this.render();
+
+        }    
+
+    });
+
+    return AccessibilityView;
+
+});
