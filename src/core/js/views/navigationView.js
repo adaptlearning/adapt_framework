8ad42bf5a07a6e25d52a3d63dfece7d38f522f85
Oliver Foster <oliver.foster@kineo.com>
2014-10-08 11:39:33 +0100

    Merged in navigation updates

--- src/core/js/views/navigationView.js
+++ src/core/js/views/navigationView.js
@@ -15,7 +15,8 @@ define(function(require) {
         className: "navigation",
         
         initialize: function() {
-            this.listenTo(Adapt, 'router:menu router:page', this.hideNavigationButton);
+            this.listenTo(Adapt, 'router:menu router:page', this.toggleNavigationButtonVisibility);
+            this.listenTo(Adapt, 'navigation:menuButton', this.navigateToParentMenu);
             this.template = "navigation";
             this.preRender();
         },
@@ -30,12 +31,30 @@ define(function(require) {
         },
         
         render: function() {
-            var template = Handlebars.templates[this.template]
-            this.$el.html(template(Adapt.course.get('_accessibility')._ariaLabels)).appendTo('#wrapper');
+            var template = Handlebars.templates[this.template];
+            var data = {
+                config: Adapt.course.get('_navigation'),
+                aria: Adapt.course.get('_accessibility')._ariaLabels
+            };
+            this.$el.html(template(data)).appendTo('#wrapper');
             _.defer(_.bind(function() {
                 Adapt.trigger('navigationView:postRender', this);
+                this.addConfigClasses();
             }, this));
             return this;
+        },
+        
+        addConfigClasses: function() {
+            var config = Adapt.course.get('_navigation');
+            if (config._showIconBorders) {
+                this.$el.addClass("show-icon-border");
+            }
+            if (config._showAllTooltipsOnHover) {
+                this.$el.addClass("show-all-tooltips-hover");
+            }
+            if (config._tooltipsAlwaysVisible) {
+                this.$el.addClass("tooltips-always-visible");
+            }
         },
         
         triggerEvent: function(event) {
@@ -44,16 +63,26 @@ define(function(require) {
             Adapt.trigger('navigation:' + currentEvent);
         },
 
-        hideNavigationButton: function(model) {
+        toggleNavigationButtonVisibility: function(model) {
             if (model.get('_type') === "course") {
-                $('.navigation-back-button').addClass('display-none');
+                $('.navigation-button').addClass('display-none');
             } else {
                 this.showNavigationButton();
             }
         },
 
         showNavigationButton: function() {
-            $('.navigation-back-button').removeClass('display-none');
+            $('.navigation-button').removeClass('display-none');
+        },
+
+        navigateToParentMenu: function() {
+            var currentModel = Adapt.findById(Adapt.location._currentId);
+            var parentId = currentModel.get("_parentId");
+            if (parentId === "course") {
+                Backbone.history.navigate("#/", {trigger: true});
+            } else {
+                Backbone.history.navigate("#/id/" + parentId, {trigger: true});
+            }
         }
         
     });
