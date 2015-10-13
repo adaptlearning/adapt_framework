7ca9300e03bdbc9bcfd68146bca6b5eac7a173d6
Thomas Taylor <thomas.taylor@kineo.com>
2015-10-13 11:59:00 +0100

    Add filter to requires-bundle

--- grunt/config/requirejs-bundle.js
+++ grunt/config/requirejs-bundle.js
@@ -1,36 +1,50 @@
-module.exports = {
-    components: {
-        src: '<%= sourcedir %>components',
-        dest: '<%= sourcedir %>components/components.js',
-        options: {
-            baseUrl: '<%= sourcedir %>',
-            moduleName: 'components/components'
-        }
-    },
-    extensions: {
-        src: '<%= sourcedir %>extensions',
-        dest: '<%= sourcedir %>extensions/extensions.js',
-        options: {
-            baseUrl: '<%= sourcedir %>',
-            moduleName: 'extensions/extensions'
-        }
-    },
-    menu: {
-        src: '<%= sourcedir %>menu/',
-        dest: '<%= sourcedir %>menu/menu.js',
-        options: {
-            include: '<%= menu %>',
-            baseUrl: '<%= sourcedir %>',
-            moduleName: 'menu/menu'
-        }
-    },
-    theme: {
-        src: '<%= sourcedir %>theme',
-        dest: '<%= sourcedir %>theme/theme.js',
-        options: {
-            include: '<%= theme %>',
-            baseUrl: '<%= sourcedir %>',
-            moduleName: 'themes/themes'
+module.exports = function(grunt) {
+    return {
+        components: {
+            src: '<%= sourcedir %>components',
+            dest: '<%= sourcedir %>components/components.js',
+            options: {
+                baseUrl: '<%= sourcedir %>',
+                moduleName: 'components/components',
+                filter: function(filepath) {
+                    return grunt.config('helpers').excludedFilter(filepath);
+                }
+            }
+        },
+        extensions: {
+            src: '<%= sourcedir %>extensions',
+            dest: '<%= sourcedir %>extensions/extensions.js',
+            options: {
+                baseUrl: '<%= sourcedir %>',
+                moduleName: 'extensions/extensions',
+                filter: function(filepath) {
+                    return grunt.config('helpers').excludedFilter(filepath);
+                }
+            }
+        },
+        menu: {
+            src: '<%= sourcedir %>menu/',
+            dest: '<%= sourcedir %>menu/menu.js',
+            options: {
+                include: '<%= menu %>',
+                baseUrl: '<%= sourcedir %>',
+                moduleName: 'menu/menu',
+                filter: function(filepath) {
+                    return grunt.config('helpers').excludedFilter(filepath);
+                }
+            }
+        },
+        theme: {
+            src: '<%= sourcedir %>theme',
+            dest: '<%= sourcedir %>theme/theme.js',
+            options: {
+                include: '<%= theme %>',
+                baseUrl: '<%= sourcedir %>',
+                moduleName: 'themes/themes',
+                filter: function(filepath) {
+                    return grunt.config('helpers').excludedFilter(filepath);
+                }
+            }
         }
     }
 }
