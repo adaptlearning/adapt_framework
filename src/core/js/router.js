3ec72fa04be917cf7c34c3b0c39709e58c3f0816
Oliver Foster <oliver.foster@kineo.com>
2015-10-16 12:49:26 +0100

    Added navigateToParent event handler

--- src/core/js/router.js
+++ src/core/js/router.js
@@ -17,6 +17,7 @@ define([
             });
             this.listenTo(Adapt, 'navigation:backButton', this.navigateToPreviousRoute);
             this.listenTo(Adapt, 'navigation:homeButton', this.navigateToHomeRoute);
+            this.listenTo(Adapt, 'navigation:parentButton', this.navigateToParent);
             this.listenTo(Adapt, "router:navigateTo", this.handleRoute);
         },
 
