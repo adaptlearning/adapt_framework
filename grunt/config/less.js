e8d7df5bf26cf9fe604e0cd0ea9c3a997016a9fb
Oliver Foster <oliver.foster@kineo.com>
2015-10-30 14:28:58 +0000

    Added custom less compiler and sourcemapper

--- grunt/config/less.js
+++ grunt/config/less.js
@@ -1,10 +1,46 @@
-module.exports = {
+module.exports = function (grunt, options) {
+    return {
+        dev: {
     options:{
-        compress:true
+                mandatory: [
+                    '<%= sourcedir %>core/less/*.less'
+                ],
+                src: [
+                    '<%= sourcedir %>menu/<%= menu %>/**/*.less',
+                    '<%= sourcedir %>components/**/*.less',
+                    '<%= sourcedir %>extensions/**/*.less',
+                    '<%= sourcedir %>theme/<%= theme %>/**/*.less'
+                ],
+                sourcemaps:true,
+		        compress:false,
+                dest: '<%= outputdir %>adapt/css/',
+                cssFilename: "adapt.css",
+                mapFilename: "adapt.css.map",
+                filter: function(filepath) {
+                    return grunt.config('helpers').includedFilter(filepath);
+                }
+            }
     },
-    dist: {
-        files: {
-            '<%= outputdir %>adapt/css/adapt.css' : '<%= sourcedir %>less/adapt.less'
+        compile: {
+            options: {
+                mandatory: [
+                    '<%= sourcedir %>core/less/*.less'
+                ],
+                src: [
+                    '<%= sourcedir %>menu/<%= menu %>/**/*.less',
+                    '<%= sourcedir %>components/**/*.less',
+                    '<%= sourcedir %>extensions/**/*.less',
+                    '<%= sourcedir %>theme/<%= theme %>/**/*.less'
+                ],
+                sourcemaps: false,
+                compress:true,
+                dest: '<%= outputdir %>adapt/css/',
+                cssFilename: "adapt.css",
+                mapFilename: "adapt.css.map",
+                filter: function(filepath) {
+                    return grunt.config('helpers').includedFilter(filepath);
+                }
+            }
         }
     }
 }
