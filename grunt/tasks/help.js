f801d07fe154f834dc225d0114ccc5cc85406a40
Thomas Taylor <thomas.taylor@kineo.com>
2016-01-07 12:16:52 +0000

    Fix help task

--- grunt/tasks/help.js
+++ grunt/tasks/help.js
@@ -1,38 +1,52 @@
+var fs = require('fs');
+var path = require('path');
 /*
 * Lists out the available tasks along with their descriptions.
 * Tasks in the array below will not be listed.
 */
 module.exports = function(grunt) {
-      grunt.registerTask('help', function() {
-          var chalk = require('chalk'); // for some nice colouring
-          var columnify = require('columnify'); // deals with formatting
-          var config = grunt.config('help') || {
-            maxConsoleWidth: '80',
-          };
+    grunt.registerTask('help', function() {
+        var chalk = require('chalk'); // for some nice colouring
+        var columnify = require('columnify'); // deals with formatting
+        var config = grunt.config('help') || {
+          maxConsoleWidth: '80',
+        };
 
-          grunt.log.writeln('');
-          grunt.log.writeln(chalk.underline('Adapt Learning automated build process'));
-          grunt.log.writeln('');
-          grunt.log.writeln('See below for the list of available tasks:');
-          grunt.log.writeln('');
+        grunt.log.writeln('');
+        grunt.log.writeln(chalk.underline('Adapt Learning automated build process'));
+        grunt.log.writeln('');
+        grunt.log.writeln('See below for the list of available tasks:');
+        grunt.log.writeln('');
 
-          var taskData = {};
-          var maxTaskLength = 0;
+        var maxTaskLength = 0;
 
-          // TODO: find way of getting task list with async task loading
+        var options = {
+            maxWidth: config.maxConsoleWidth - maxTaskLength,
+            showHeaders: false,
+            columnSplitter: '  '
+        };
 
-          var options = {
-              maxWidth: config.maxConsoleWidth - maxTaskLength,
-              showHeaders: false,
-              columnSplitter: '  '
-          };
+        // log everything
+        grunt.log.writeln(columnify(getTaskData(), options));
 
-          // log everything
-          grunt.log.writeln(columnify(taskData, options));
+        grunt.log.writeln('');
+        grunt.log.writeln('Run a task using: grunt [task name]');
+        grunt.log.writeln('');
+        grunt.log.writeln('For more information, see https://github.com/adaptlearning/adapt_framework/wiki');
+    });
+}
 
-          grunt.log.writeln('');
-          grunt.log.writeln('Run a task using: grunt [task name]');
-          grunt.log.writeln('');
-          grunt.log.writeln('For more information, see https://github.com/adaptlearning/adapt_framework/wiki');
-      });
-  }
+// TODO: this only includes tasks in /tasks...might not be good enough
+function getTaskData() {
+    var taskData = {};
+    var files = fs.readdirSync(__dirname);
+    var re = new RegExp(/grunt.register(Multi)?Task\('(.+)', '(.*)',/);
+
+    for(var i = 0, count = files.length; i < count; i++) {
+        var file = fs.readFileSync(path.join(__dirname, files[i]), 'utf8');
+        var match = file.match(re);
+        if(match) taskData[match[2]] = match[3] || '';
+    }
+
+    return taskData;
+}
