86d37f2b498a8973d81cb909de5d169ee3d031a2
Andy Bell <andrewb@learningpool.com>
2016-02-15 11:51:35 +0000

    Added support for string substitution in XML files
    This is required to support upcoming work on the xAPI plugin but will also work with a slightly modifieed Spoor plugin.  The placeholder format is: @@property.
    
    Right now properties from config.json and course.json are supported.
    
    Squashed commit messages:
    - Added replace task to build.js and dev.js
    - Added support for passing both course and config to replace call
    - Added 'description' to course.json.  This attribute is useful for the SCORM and xAPI extensions.
    - Added shim for missing xAPI property. This is to cover edge cases where the xAPI extension may be defined but its configuration may be incomplete.
    - Added a warning mesage for when xAPI activityID has not been set.  The course will not confirm to the spec if this has been left empty.

---
+++ grunt/config/replace.js
@@ -0,0 +1,56 @@
+module.exports = function (grunt, options) {
+
+  var courseDir = options.sourcedir + 'course/';
+
+  if (options.outputdir.indexOf('build/') > -1) {
+    courseDir = options.outputdir + 'course/';
+  }
+
+  var configJson = grunt.file.readJSON(courseDir + 'config.json');
+  var defaultLanguage = configJson._defaultLanguage || 'en';
+  var courseJson = grunt.file.readJSON(courseDir + defaultLanguage + '/course.json');
+
+  // Backwards compatibility for courses missing 'description'
+  if (!courseJson.hasOwnProperty('description')) {
+    courseJson.description = '';
+  }
+
+  // A shim for edge cases where xAPI has not been configured.
+  if (!configJson.hasOwnProperty('_xapi')) {
+    configJson._xapi = {
+      'activityID': '',
+      '_isEnabled': false
+    };
+  } else {
+    // xAPI has been enabled, check if the activityID has been set.
+    if (configJson._xapi.hasOwnProperty('activityID') && configJson._xapi.activityID == '') {
+      grunt.log.writeln('WARNING: xAPI activityID has not been set');
+    }
+  }
+
+  // Combine the course and config JSON so both can be passet to replace.  
+  var json = {
+    'course': courseJson,
+    'config': configJson
+  };
+
+  return {
+    dist: {
+      options: {
+        patterns: [
+          {
+            json: json
+          }
+        ]
+      },
+      files: [
+        {
+          expand: true,
+          flatten: true,
+          src: [options.outputdir + '*.xml'],
+          dest: options.outputdir
+        }
+      ]
+    }
+  }
+}
