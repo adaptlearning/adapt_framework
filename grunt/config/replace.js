b8421676f51aad20fc96ce89b1b2c88f120890cd
Brian Quinn <brian@learningpool.com>
2016-02-16 11:19:31 +0000

    Added support for passing both course and config to replace call

--- grunt/config/replace.js
+++ grunt/config/replace.js
@@ -1,21 +1,27 @@
 module.exports = function (grunt, options) {
 
   var courseDir = options.sourcedir + 'course/';
+
   if (options.outputdir.indexOf('courses')) {
     courseDir = options.outputdir + 'course/';
   }
 
   var configJson = grunt.file.readJSON(courseDir + 'config.json');
-  var defaultLanguage = configJson._defaultLanguage || 'en'
-  var pathToCourseJson = courseDir + defaultLanguage + '/course.json';
-  var courseJSON = grunt.file.readJSON(pathToCourseJson);
-  
+  var defaultLanguage = configJson._defaultLanguage || 'en';
+  var courseJson = grunt.file.readJSON(courseDir + defaultLanguage + '/course.json');
+
+  // Combine the course and config JSON so both can be passet to replace.  
+  var json = {
+    'course': courseJson,
+    'config': configJson
+  };
+
   return {
     dist: {
       options: {
         patterns: [
           {
-            json: courseJSON
+            json: json
           }
         ]
       },
