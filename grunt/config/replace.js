88a8f0505649018a87f649c949dc9adc077c3047
Brian Quinn <brian@learningpool.com>
2016-03-01 11:53:59 +0000

    Resolves #984, removes dependency on 'build' folder existing
    Also using path.join() now rather than string concatenation for folder names.

--- grunt/config/replace.js
+++ grunt/config/replace.js
@@ -1,19 +1,46 @@
+var path = require('path');
+var fs = require('fs');
+
 module.exports = function (grunt, options) {
 
-  var courseDir = options.sourcedir + 'course/';
+  var courseDir = path.join(options.sourcedir, 'course');
 
-  if (options.outputdir.indexOf('build/') > -1) {
-    courseDir = options.outputdir + 'course/';
+  if (options.outputdir.indexOf('build') > -1) {
+    courseDir = path.join(options.outputdir, 'course');
   }
 
-  var configJson = grunt.file.readJSON(courseDir + 'config.json');
+  var pathToConfig = path.join(courseDir, 'config.json')
+
+  try {
+    // Verify that the configuration file exists.
+    fs.accessSync(pathToConfig);
+  } catch(ex) {
+    // If it does not, early return an empty configuration object, 
+    // so the replace task can still run.
+    return {
+      dist: {
+        options: {
+          patterns: [
+            {
+              json: {}
+            }
+          ]
+        },
+        files: [
+          {
+            expand: true,
+            flatten: true,
+            src: [options.outputdir + '*.xml'],
+            dest: options.outputdir
+          }
+        ]
+      }
+    }
+  }
+  
+  var configJson = grunt.file.readJSON(pathToConfig);
   var defaultLanguage = configJson._defaultLanguage || 'en';
-  var courseJson = grunt.file.readJSON(courseDir + defaultLanguage + '/course.json');
-
-  // Backwards compatibility for courses missing 'description'
-  if (!courseJson.hasOwnProperty('description')) {
-    courseJson.description = '';
-  }
+  var courseJson = grunt.file.readJSON(path.join(courseDir, defaultLanguage, 'course.json'));
 
   // A shim for edge cases where xAPI has not been configured.
   if (!configJson.hasOwnProperty('_xapi')) {
