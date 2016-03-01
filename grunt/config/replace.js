bb83079e115bcc1d5a4afa2fe96b09eae304dba6
Brian Quinn <brian@learningpool.com>
2016-03-01 16:37:56 +0000

    Re-worked replace.js
    Added 'silent: true' to suppress replace logs.
    
    Corrected _activityID property name.

--- grunt/config/replace.js
+++ grunt/config/replace.js
@@ -14,15 +14,42 @@ module.exports = function (grunt, options) {
   try {
     // Verify that the configuration file exists.
     fs.accessSync(pathToConfig);
-  } catch(ex) {
-    // If it does not, early return an empty configuration object, 
-    // so the replace task can still run.
+    
+    var configJson = grunt.file.readJSON(pathToConfig);
+    var defaultLanguage = configJson._defaultLanguage || 'en';
+    var courseJson = grunt.file.readJSON(path.join(courseDir, defaultLanguage, 'course.json'));
+    
+    // Backwards compatibility for courses missing 'description'
+    if (!courseJson.hasOwnProperty('description')) {
+      courseJson.description = '';
+    }
+
+    // A shim for edge cases where xAPI has not been configured.
+    if (!configJson.hasOwnProperty('_xapi')) {
+      configJson._xapi = {
+        '_activityID': '',
+        '_isEnabled': false
+      };
+    } else {
+      // xAPI has been enabled, check if the activityID has been set.
+      if (configJson._xapi.hasOwnProperty('_activityID') && configJson._xapi._activityID == '') {
+        grunt.log.writeln('WARNING: xAPI activityID has not been set');
+      }
+    }
+
+    // Combine the course and config JSON so both can be passed to replace.  
+    var json = {
+      'course': courseJson,
+      'config': configJson
+    };
+
     return {
       dist: {
         options: {
+          silent: true,
           patterns: [
             {
-              json: {}
+              json: json
             }
           ]
         },
@@ -30,54 +57,23 @@ module.exports = function (grunt, options) {
           {
             expand: true,
             flatten: true,
-            src: [options.outputdir + '*.xml'],
+            src: [path.join(options.outputdir, '*.xml')],
             dest: options.outputdir
           }
         ]
       }
     }
-  }
-  
-  var configJson = grunt.file.readJSON(pathToConfig);
-  var defaultLanguage = configJson._defaultLanguage || 'en';
-  var courseJson = grunt.file.readJSON(path.join(courseDir, defaultLanguage, 'course.json'));
-
-  // A shim for edge cases where xAPI has not been configured.
-  if (!configJson.hasOwnProperty('_xapi')) {
-    configJson._xapi = {
-      'activityID': '',
-      '_isEnabled': false
-    };
-  } else {
-    // xAPI has been enabled, check if the activityID has been set.
-    if (configJson._xapi.hasOwnProperty('activityID') && configJson._xapi.activityID == '') {
-      grunt.log.writeln('WARNING: xAPI activityID has not been set');
-    }
-  }
-
-  // Combine the course and config JSON so both can be passed to replace.  
-  var json = {
-    'course': courseJson,
-    'config': configJson
-  };
-
-  return {
-    dist: {
-      options: {
-        patterns: [
-          {
-            json: json
-          }
-        ]
-      },
-      files: [
-        {
-          expand: true,
-          flatten: true,
-          src: [options.outputdir + '*.xml'],
-          dest: options.outputdir
-        }
-      ]
+  } catch(ex) {
+    // If it does not, early return an empty configuration object, 
+    // so the replace task can still run.
+    return {
+      dist: {
+        options: {
+          silent: true,
+          patterns: []
+        },
+        files: []
+      }
     }
   }
 }
