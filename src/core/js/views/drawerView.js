0d5ba8121cebca0b44240fec9527205e37bc54f7
Sarveshwar Gavhane <sarveshwar.gavhane@exultcorp.com>
2016-04-15 11:48:17 +0530

    fixes for https://github.com/adaptlearning/adapt_framework/issues/895

--- src/core/js/views/drawerView.js
+++ src/core/js/views/drawerView.js
@@ -177,8 +177,8 @@ define(function(require) {
                 Adapt.trigger('drawer:opened');
                 
                 //focus on first tabbable element in drawer
-                this.$el.a11y_focus();
-			}
+                this.$el.find('.drawer-close-button').attr('tabindex',-1).a11y_focus();
+	    }
 
         },
 
