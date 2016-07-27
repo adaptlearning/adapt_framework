898c29c7eb35d6174e6edfc3b32d42d8143ce586
Oliver Foster <oliver.foster@kineo.com>
2016-07-27 10:04:45 +0100

    issue-1164: moved theme common assets to core (#1166)

--- grunt/config/copy.js
+++ grunt/config/copy.js
@@ -31,6 +31,19 @@ module.exports = function (grunt, options) {
                 }
             ]
         },
+        coreAssets: {
+            files: [
+                {
+                    expand: true,
+                    src: ['<%= sourcedir %>core/assets/**'],
+                    dest: '<%= outputdir %>assets/',
+                    filter: function(filepath) {
+                        return grunt.config('helpers').includedFilter(filepath);
+                    },
+                    flatten: true
+                }
+            ]
+        },
         componentAssets: {
             files: [
                 {
