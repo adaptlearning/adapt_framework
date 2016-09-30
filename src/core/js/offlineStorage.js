5d9da97738bae02e67493edfec9bdfe3cbfb396f
moloko <mleathes+github@gmail.com>
2016-09-30 17:38:33 +0100

    allow offlineStorage to be 'ready' when contrib-spoor is not installed (see https://github.com/adaptlearning/adapt-contrib-languagePicker/issues/14)

--- src/core/js/offlineStorage.js
+++ src/core/js/offlineStorage.js
@@ -7,9 +7,18 @@ define([
 
 	Adapt.offlineStorage = {
 
-		ready: false,
+		/**
+		 * set to true initially so that if there are no offlineStorage handlers (i.e. if contrib-spoor is not installed)
+		 * this can still be accessed OK
+		 */
+		ready: true,
 
+		/**
+		 * set .ready to false if an offlineStorage handler is being attached - we'll need to wait until the handler lets us know
+		 * it's ready before we can safely use offlineStorage
+		 */
 		initialize: function(handler) {
+			ready = false;
 			this._handler = handler;
 		},
 
