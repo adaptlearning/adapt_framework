2da774754e47b219dbe48d48350e8c51d9b8b779
Sarveshwar Gavhane <sarveshwar.gavhane@exultcorp.com>
2017-01-12 18:07:11 +0530

    added path for spoorOffline

--- grunt/config/connect.js
+++ grunt/config/connect.js
@@ -1,7 +1,8 @@
 module.exports = function (grunt, options) {
 
   var port = grunt.option('port') || 9001;
-
+  var host = grunt.option('host') || "localhost";
+  
   return {
     server: {
       options: {
@@ -16,7 +17,7 @@ module.exports = function (grunt, options) {
         port: port,
         base: '<%= outputdir %>',
         keepalive:true,
-        open:true
+        path: 'http://'+host+':'+port+'/scorm_test_harness.html'
       }
     }
   }
