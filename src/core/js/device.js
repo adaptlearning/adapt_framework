ca59e7abe5f3a4e602de2d247aedc26e37c5f161
lc-thomasberger <thomas.berger@learnchamp.com>
2017-03-22 10:34:38 +0100

    Turn browserstring into valid css classes (#1476)
    * Turn browserstring into valid css classes
    - replace "." with "-"
    - convert to lowercase
    
    * updated to keep the old syntax

--- src/core/js/device.js
+++ src/core/js/device.js
@@ -190,6 +190,7 @@ define(function(require) {
     // Convert 'msie' to 'ie' for backwards compatibility
     var browserString = (Adapt.device.browser.toLowerCase() === 'msie') ? 'ie' : Adapt.device.browser.toLowerCase();
     browserString = browserString + ' version-' + Adapt.device.version + ' OS-' + Adapt.device.OS + ' ' + getAppleDeviceType();
+    browserString += browserString.replace('.', '-').toLowerCase();
 
     $("html").addClass(browserString + ' pixel-density-' + pixelDensity());
 });
