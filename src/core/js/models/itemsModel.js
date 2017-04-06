aa84bae9083cd1056051e89e894a9182c96eab7a
Thomas Berger <thomas.berger@learnchamp.com>
2017-04-06 14:11:47 +0200

    use function to manipulate items _isActive state

--- src/core/js/models/itemsModel.js
+++ src/core/js/models/itemsModel.js
@@ -13,6 +13,7 @@ define([
         resetItems: function() {
             _.each(this.get('_items'), function(item) {
                 item._isVisited = false;
+                item._isActive = false;
             });
         },
         
@@ -30,7 +31,7 @@ define([
             });
         },
 
-        setItemAsVisited: function(index) {
+        setItemAtIndexAsVisited: function(index) {
             var item = this.get('_items')[index];
             item._isVisited = true;
         },
@@ -43,6 +44,61 @@ define([
             if (this.getCompletionStatus()) {
                 this.setCompletionStatus();
             }
+        },
+
+        resetActiveItems: function(trigger) {
+            var _trigger = (trigger === undefined) ? true : trigger;
+            var items = this.get('_items');
+
+            _.each(items, function(item) {
+                item._isActive = false;
+            });
+
+            if (_trigger) this.trigger('change:_items:_isActive', this, items);
+        },
+
+        getActiveItems: function() {
+            return _.filter(this.get('_items'), function(item) {
+                return item._isActive;
+            });
+        },
+        
+        getActiveItemsCount: function() {
+            return this.getActiveItems().length;
+        },
+        
+        getActiveItemsIndexes: function() {
+            var indexes = [];
+            var items = this.get('_items');
+            
+            for (var i = 0, l = items.length; i < l; i++)
+                if (items[i]._isActive) indexes.push(i); 
+
+            return indexes;
+        },
+
+        setItemAtIndexAsActive: function(index, trigger) {
+            var _trigger = (trigger === undefined) ? true : trigger;
+            var items = this.get('_items');
+            var item = items[index];
+            
+            if (item === undefined) return false;
+
+            item._isActive = true;
+            if (_trigger) this.trigger('change:_items:_isActive', this, items);
+            return item;
+        },
+        
+        setItemAtIndexAsInactive: function(index, trigger) {
+            var _trigger = (trigger === undefined) ? true : trigger;
+            var items = this.get('_items');
+            var item = items[index];
+            
+            if (item === undefined) return false;
+
+            item._isActive = false;
+            if (_trigger) this.trigger('change:_items:_isActive', this, items);
+            return item;
         }
 
     });
