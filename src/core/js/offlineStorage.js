a18ae83a2e4cff1d72e90fe64918aaf83195e802
Ciaran Duffy <ciaran@learningpool.com>
2017-04-07 11:22:18 +0100

    Fixes #1510, problem with Adapt.offlineStorage (#1511)
    This was never setting ready to true.
    
    Converted tabs to spaces as well.

--- src/core/js/offlineStorage.js
+++ src/core/js/offlineStorage.js
@@ -1,50 +1,54 @@
 define([
-	'core/js/adapt'
+    'core/js/adapt'
 ], function(Adapt) {
 
-	//Basic API for setting and getting name+value pairs
-	//Allows registration of a single handler.
+    //Basic API for setting and getting name+value pairs
+    //Allows registration of a single handler.
 
-	var OfflineStorage = Backbone.Controller.extend({
+    var OfflineStorage = Backbone.Controller.extend({
 
-		/**
-		 * set to true initially so that if there are no offlineStorage handlers (i.e. if contrib-spoor is not installed)
-		 * this can still be accessed OK
-		 */
-		ready: true,
-		_handler: undefined,
+        /**
+         * set to true initially so that if there are no offlineStorage handlers (i.e. if contrib-spoor is not installed)
+         * this can still be accessed OK
+         */
+        ready: true,
+        _handler: undefined,
 
-		/**
-		 * set .ready to false if an offlineStorage handler is being attached - we'll need to wait until the handler lets us know
-		 * it's ready before we can safely use offlineStorage
-		 */
-		initialize: function(handler) {
-			this.ready = false;
-			this._handler = handler;
-		},
+        /**
+         * set .ready to false if an offlineStorage handler is being attached - we'll need to wait until the handler lets us know
+         * it's ready before we can safely use offlineStorage
+         */
+        initialize: function(handler) {
+            if (!handler) {
+                return;
+            }
 
-		set: function(name, value) {
-			if (!(this._handler && this._handler.set)) return;
-			return this._handler.set.apply(this._handler, arguments);
-		},
+            this.ready = false;
+            this._handler = handler;
+        },
 
-		get: function(name) {
-			if (!(this._handler && this._handler.get)) return;
-			return this._handler.get.apply(this._handler, arguments);
-		},
+        set: function(name, value) {
+            if (!(this._handler && this._handler.set)) return;
+            return this._handler.set.apply(this._handler, arguments);
+        },
 
-		/**
-		 * Some forms of offlineStorage could take time to initialise, this allows us to let plugins know when it's ready to be used 
-		 */
-		setReadyStatus: function() {
-			this.ready = true;
-			Adapt.trigger("offlineStorage:ready");
-		}
+        get: function(name) {
+            if (!(this._handler && this._handler.get)) return;
+            return this._handler.get.apply(this._handler, arguments);
+        },
 
-	});
+        /**
+         * Some forms of offlineStorage could take time to initialise, this allows us to let plugins know when it's ready to be used
+         */
+        setReadyStatus: function() {
+            this.ready = true;
+            Adapt.trigger("offlineStorage:ready");
+        }
 
-	Adapt.offlineStorage = new OfflineStorage();
+    });
 
-	return Adapt.offlineStorage;
+    Adapt.offlineStorage = new OfflineStorage();
+
+    return Adapt.offlineStorage;
 
 });
