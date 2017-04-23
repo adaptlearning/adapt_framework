d0dddb71e7f486f56496a04901296b22fffcd285
Chris Steele <chris.steele@kineo.com>
2017-04-23 11:54:53 +0700

    Undo accidental commit to master

--- src/core/js/app.js
+++ src/core/js/app.js
@@ -118,15 +118,6 @@ require([
             Adapt.log.error('Error during app:dataReady trigger', e);
         }
 
-        if (!Adapt.isWaitingForPlugins()) triggerInitialize();
-        else Adapt.once('plugins:ready', function() {
-            triggerInitialize();
-        });
-    }
-
-    function triggerInitialize() {
-        Adapt.log.debug('Calling Adapt.initialize');
-
         Adapt.navigation = new NavigationView();// This should be triggered after 'app:dataReady' as plugins might want to manipulate the navigation
 
         Adapt.initialize();
