fe4472cdc068c3aef1a3a5813e0385712095e285
Nayan Khedkar <nayan.khedkar@exultcorp.com>
2017-07-05 11:52:59 +0530

    removed shim functionality

--- src/core/js/scriptLoader.js
+++ src/core/js/scriptLoader.js
@@ -53,37 +53,11 @@
         if(window.jQuery === undefined) {
             setTimeout(checkJQueryStatus, 100);
         } else {
-            loadShim();
+            loadFoundationLibraries();
         }
     }
 
-    //5. Load IE 8 shim
-    function loadShim() {
-
-        var isIE8 = (IE == 8);
-
-        Modernizr.load([
-            {
-                test: isIE8,
-                yep: 'libraries/es5-shim.min.js',
-                nope: '',
-                complete: loadFoundationLibraries()
-            }
-        ]);
-
-        if (isIE8) {
-            fixIE8ConsoleLog();
-        }
-        
-    }
-
-    function fixIE8ConsoleLog() {
-
-        console.log = Function.prototype.call.bind(console.log, console);
-
-    }
-
-    //6. Load foundation libraries and templates
+    //5. Load foundation libraries and templates
     function loadFoundationLibraries() {
         require([
             "underscore",
@@ -101,7 +75,7 @@
         ], loadAdapt);
     }
 
-    //7. Load adapt
+    //6. Load adapt
     function loadAdapt() {
         switch (IE) {
         case 8: case 9:
