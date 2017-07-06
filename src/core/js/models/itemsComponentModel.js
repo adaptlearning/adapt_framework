6bd5a67fc0d4fb89decad7e26be3b7652ed63300
tomgreenfield <tomgreenfield@users.noreply.github.com>
2017-07-06 11:14:26 +0100

    Add items collection

---
+++ src/core/js/models/itemsComponentModel.js
@@ -0,0 +1,67 @@
+define([
+    'core/js/models/componentModel',
+    'core/js/models/itemModel'
+], function(ComponentModel, ItemModel) {
+
+    var ItemsComponentModel = ComponentModel.extend({
+
+        init: function() {
+            this.setUpItems();
+            this.listenTo(this.get('_items'), {
+                'change:_isVisited': this.checkCompletionStatus
+            });
+        },
+
+        setUpItems: function() {
+            var items = this.get('_items');
+            var collection = new Backbone.Collection(null, { model: ItemModel });
+
+            items.forEach(function(item, index) {
+                item._index = index;
+                collection.add(item);
+            });
+
+            this.set('_items', collection);
+        },
+
+        getItem: function(index) {
+            return this.get('_items').findWhere({ _index: index });
+        },
+
+        getVisitedItems: function() {
+            return this.get('_items').where({ _isVisited: true });
+        },
+
+        getActiveItems: function() {
+            return this.get('_items').where({ _isActive: true });
+        },
+
+        getActiveItem: function() {
+            return this.get('_items').findWhere({ _isActive: true });
+        },
+
+        areAllItemsCompleted: function() {
+            return this.getVisitedItems().length === this.get('_items').length;
+        },
+
+        checkCompletionStatus: function() {
+            if (this.areAllItemsCompleted()) {
+                this.setCompletionStatus();
+            }
+        },
+
+        reset: function(type, force) {
+            this.get('_items').each(function(item) { item.reset(); });
+
+            ComponentModel.prototype.reset.call(this, type, force);
+        },
+
+        resetActiveItems: function() {
+            this.get('_items').each(function(item) { item.toggleActive(false); });
+        }
+
+    });
+
+    return ItemsComponentModel;
+
+});
