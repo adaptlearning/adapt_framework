aa41d95c55508726fe835531a5bc18989cf7e9ca
Brian Quinn <brian@learningpool.com>
2017-08-17 16:06:36 +0100

    Resolves #1701, removes unnecessary XML manifest files
    If using xAPI, cmi5.xml is removed.  If using cmi5, tincan.xml is removed.

--- grunt/config/replace.js
+++ grunt/config/replace.js
@@ -37,6 +37,18 @@ module.exports = function (grunt, options) {
         if (configJson._xapi.hasOwnProperty('_activityID') && configJson._xapi._activityID == '') {
           grunt.log.writeln('WARNING: xAPI activityID has not been set');
         }
+
+        // cmi5 is a profile of the xAPI specification and some systems, e.g. SCORM Cloud
+        // do not work well with both manifest types, so remove the one which is not being used.
+        if (configJson._xapi.hasOwnProperty('_specification')) {
+          if (configJson._xapi._specification === 'xAPI') {
+            // Remove the cmi5.xml file.
+            grunt.file.delete(path.join(options.outputdir, 'cmi5.xml'));
+          } else if (configJson._xapi._specification === 'cmi5') {
+            // Remove the tincan.xml file.
+            grunt.file.delete(path.join(options.outputdir, 'tincan.xml'));
+          }
+        }
       }
 
       // Ensure that only whitelisted attributes can be replaced.
