e09ec48bffc509b309f254a2debaa009e2d6dc5c
Oliver Foster <oliver.foster@kineo.com>
2017-08-18 16:09:58 +0100

    issue/1741: make scripts asynchronous

--- grunt/tasks/scripts.js
+++ grunt/tasks/scripts.js
@@ -7,29 +7,44 @@ module.exports = function(grunt) {
         var path = require("path");
         var fs = require("fs");
         var options = this.options({});
+        var chalk = require("chalk");
+        var async = require("async");
+        var taskCallback = this.async();
 
         if (options.plugins) {
 
+            var paths = [];
             for (var i = 0, l = options.plugins.length; i < l; i++) {
                 var src = options.plugins[i];
-                grunt.file.expand({ filter: options.pluginsFilter }, src).forEach(function(bowerJSONPath) {
+                paths = paths.concat(grunt.file.expand({ filter: options.pluginsFilter }, src));
+            }
 
-                    if (bowerJSONPath === undefined) return;
-                    var bowerJSON = grunt.file.readJSON(bowerJSONPath);
+            async.each(paths, function(bowerJSONPath, done) {
 
-                    if (!bowerJSON.scripts) return;
+                if (bowerJSONPath === undefined) return done();
+                var bowerJSON = grunt.file.readJSON(bowerJSONPath);
 
-                    var plugindir = path.dirname(bowerJSONPath)
-                    var script = bowerJSON.scripts[mode];
+                if (!bowerJSON.scripts) return done();
+
+                var plugindir = path.dirname(bowerJSONPath)
+                var script = bowerJSON.scripts[mode];
+
+                if (!script) return done();
+
+                try {
                     var buildModule = require(path.join(plugindir, script));
                     buildModule(fs, path, grunt.log.writeln, {
                        sourcedir: options.sourcedir,
                        outputdir: options.outputdir,
                        plugindir: plugindir
-                    });
+                    }, done);
+                } catch(err) {
+                    grunt.log.writeln(chalk.red(err));
+                    done();
+                }
 
-                });
-            }
+            }, taskCallback);
+
         }
     });
 
