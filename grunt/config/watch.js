9da797538993817ca611d925033adcc53efbba4e
Akshay Bhavsar <akshay.bhavsar@exultcorp.com>
2017-08-22 15:05:08 +0530

    Update watch.js

--- grunt/config/watch.js
+++ grunt/config/watch.js
@@ -27,7 +27,6 @@ module.exports = {
             '!<%= sourcedir %>core/js/scriptLoader.js',
             '!<%= sourcedir %>core/js/libraries/require.min.js',
             '!<%= sourcedir %>core/js/libraries/modernizr.js',
-            '!<%= sourcedir %>core/js/libraries/consoles.min.js',
             '!<%= sourcedir %>core/js/libraries/jquery.min.js',
             '!<%= sourcedir %>core/js/libraries/jquery.v2.min.js'
         ],
@@ -77,7 +76,6 @@ module.exports = {
         files: [
             '<%= sourcedir %>core/js/libraries/require.min.js',
             '<%= sourcedir %>core/js/libraries/modernizr.js',
-            '<%= sourcedir %>core/js/libraries/consoles.min.js',
             '<%= sourcedir %>core/js/libraries/jquery.min.js',
             '<%= sourcedir %>core/js/libraries/jquery.v2.min.js'
         ],
