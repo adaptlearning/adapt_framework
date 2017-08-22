89582dc9e67c989b17323726101d2eb449b20495
Oliver Foster <oliver.foster@kineo.com>
2017-08-22 09:52:58 +0100

    issue/1741: grunt - added scripts task adaptpostbuild

--- grunt/helpers.js
+++ grunt/helpers.js
@@ -41,7 +41,6 @@ module.exports = function(grunt) {
     });
 
     // privates
-
     var generateIncludedRegExp = function() {
         var includes = grunt.config('includes') || [];
         var re = '';
@@ -58,6 +57,16 @@ module.exports = function(grunt) {
         for(var i = 0, count = excludes.length; i < count; i++) {
             re += '\/' + excludes[i].toLowerCase() + '\/';
             if(i < excludes.length-1) re += '|';
+        }
+        return new RegExp(re, "i");
+    };
+
+    var generateScriptSafeRegExp = function() {
+        var includes = grunt.config('scriptSafe') || [];
+        var re = '';
+        for(var i = 0, count = includes.length; i < count; i++) {
+            re += '\/' + includes[i].toLowerCase() + '\/';
+            if(i < includes.length-1) re += '|';
         }
         return new RegExp(re, "i");
     };
@@ -87,6 +96,10 @@ module.exports = function(grunt) {
             'extensions',
             'menu',
             'theme'
+        ],
+        scriptSafe: [
+            'adapt-contrib-xapi',
+            'adapt-contrib-spoor'
         ]
     };
 
@@ -116,6 +129,10 @@ module.exports = function(grunt) {
 
     exports.generateConfigData = function() {
 
+        var root = __dirname.split(path.sep).slice(0,-1).join(path.sep);
+        var sourcedir = appendSlash(grunt.option('sourcedir')) || exports.defaults.sourcedir;
+        var outputdir = appendSlash(grunt.option('outputdir')) || exports.defaults.outputdir;
+
         var languageFolders = "";
         if (grunt.option('languages') && grunt.option('languages').split(',').length > 1) {
           languageFolders = "{" + grunt.option('languages') + "}";
@@ -123,30 +140,30 @@ module.exports = function(grunt) {
           languageFolders = grunt.option('languages');
         }
 
-        var data = {
-            root: __dirname.split(path.sep).slice(0,-1).join(path.sep),
-            sourcedir: appendSlash(grunt.option('sourcedir')) || exports.defaults.sourcedir,
-            outputdir: appendSlash(grunt.option('outputdir')) || exports.defaults.outputdir,
-            theme: grunt.option('theme') || exports.defaults.theme,
-            menu: grunt.option('menu') || exports.defaults.menu,
-            languages: languageFolders || exports.defaults.languages
-        };
-
         // Selectively load the course.json ('outputdir' passed by server-build)
-        var outputdir = grunt.option('outputdir') ? data.outputdir : data.sourcedir;
+        var configDir = grunt.option('outputdir') ? outputdir : sourcedir;
         // add root path if necessary, and point to course/config.json
-        var configPath = path.join(path.resolve(data.root, outputdir), 'course', 'config.json');
-
+        var configPath = path.join(path.resolve(root, configDir), 'course', 'config.json');
         try {
             var buildConfig = require(configPath).build;
-        }
-        catch(error) {
+        } catch(error) {
             return grunt.log.error(error);
         }
 
+        var data = {
+            root: root,
+            sourcedir: sourcedir,
+            outputdir: outputdir,
+            theme: grunt.option('theme') || exports.defaults.theme,
+            menu: grunt.option('menu') || exports.defaults.menu,
+            languages: languageFolders || exports.defaults.languages,
+            scriptSafe: exports.defaults.scriptSafe
+        };
+
         if(buildConfig) {
-            if(buildConfig.includes) data.includes = exports.getIncludes(buildConfig.includes, data);
-            if(buildConfig.excludes) data.excludes = buildConfig.excludes;
+            if (buildConfig.includes) data.includes = exports.getIncludes(buildConfig.includes, data);
+            if (buildConfig.excludes) data.excludes = buildConfig.excludes;
+            if (buildConfig.scriptSafe) data.scriptSafe = buildConfig.scriptSafe.split(",").map(function(item) { return item.trim() });
         }
 
         return data;
@@ -195,6 +212,23 @@ module.exports = function(grunt) {
         }
     };
 
+    exports.isPluginScriptSafe = function(pluginPath) {
+
+        pluginPath = pluginPath.replace(convertSlashes, "/");
+        var includes = grunt.config('scriptSafe');
+        var isExplicitlyDefined = (includes && pluginPath.search(exports.getScriptSafeRegExp()) !== -1);
+        var isIncluded = grunt.option('allowscripts') || includes[0] === "*" || isExplicitlyDefined;
+
+        if (!isIncluded) {
+            //grunt.log.writeln('Excluded ' + chalk.red(pluginPath));
+        } else {
+            //grunt.log.writeln('Included ' + chalk.green(pluginPath));
+        }
+
+        return isIncluded;
+     
+    };
+
     exports.includedFilter = function(filepath) {
         return exports.isPluginIncluded(filepath);
     };
@@ -212,6 +246,15 @@ module.exports = function(grunt) {
     exports.getExcludedRegExp = function() {
         var configValue = grunt.config('excludedRegExp');
         return configValue || grunt.config('excludedRegExp', generateExcludedRegExp());
+    };
+
+    exports.getScriptSafeRegExp = function() {
+        var configValue = grunt.config('scriptSafeRegExp');
+        return configValue || grunt.config('scriptSafeRegExp', generateScriptSafeRegExp());
+    };
+
+    exports.scriptSafeFilter = function(filepath) {
+        return exports.isPluginScriptSafe(filepath);
     };
 
     return exports;
