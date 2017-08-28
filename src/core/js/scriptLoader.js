81318a138400282f799c1e57fd058b39b04c5212
Nayan Khedkar <nayan.khedkar@exultcorp.com>
2017-08-28 11:47:14 +0530

    Resolve conflicts
    reference PR: https://github.com/adaptlearning/adapt_framework/pull/1635

--- src/core/js/scriptLoader.js
+++ src/core/js/scriptLoader.js
@@ -1,13 +1,5 @@
 (function() {
 
-    //Test for ie8
-    var IE = (function() {
-        if (document.documentMode) {
-            return document.documentMode;
-        }
-        return false;
-    })();
-
     //2. Setup require for old style module declarations
     function setupRequireJS() {
         requirejs.config({
@@ -20,6 +12,7 @@
                 }
             },
             paths: {
+                jquery: 'libraries/jquery.v2.min',
                 underscore: 'libraries/underscore.min',
                 backbone: 'libraries/backbone.min',
                 'backbone.controller': 'libraries/backbone.controller',
@@ -34,59 +27,13 @@
                 jqueryMobile: 'libraries/jquery.mobile.custom'
             }
         });
-        loadJQuery();
+        loadFoundationLibraries();
     }
-
-    //3. Load jquery
-    function loadJQuery() {
-        Modernizr.load([
-            {
-                test: IE == 8,
-                yep: 'libraries/jquery.min.js',
-                nope: 'libraries/jquery.v2.min.js',
-                complete: checkJQueryStatus
-            }
-        ]);
-    }
-
-    //4. Wait until JQuery gets loaded completly
-    function checkJQueryStatus() {
-        if(window.jQuery === undefined) {
-            setTimeout(checkJQueryStatus, 100);
-        } else {
-            loadShim();
-        }
-    }
-
-    //5. Load IE 8 shim
-    function loadShim() {
-
-        var isIE8 = (IE == 8);
-
-        Modernizr.load([
-            {
-                test: isIE8,
-                yep: 'libraries/es5-shim.min.js',
-                nope: '',
-                complete: loadFoundationLibraries()
-            }
-        ]);
-
-        if (isIE8) {
-            fixIE8ConsoleLog();
-        }
-
-    }
-
-    function fixIE8ConsoleLog() {
-
-        console.log = Function.prototype.call.bind(console.log, console);
-
-    }
-
-    //6. Load foundation libraries and templates
+    
+    //3. Load foundation libraries and templates
     function loadFoundationLibraries() {
         require([
+            'jquery',
             'underscore',
             'backbone',
             'backbone.controller',
@@ -103,18 +50,11 @@
         ], loadAdapt);
     }
 
-    //7. Load adapt
+    //4. Load adapt
     function loadAdapt() {
-        switch (IE) {
-            case 8: case 9:
-            //ie8 and ie9 don't do crossdomain with jquery normally
-            break;
-            default:
-                //cross domain support for all other browers
-                $.ajaxPrefilter(function( options ) {
-                    options.crossDomain = true;
-                });
-        }
+        $.ajaxPrefilter(function( options ) {
+            options.crossDomain = true;
+        });
         Modernizr.load('adapt/js/adapt.min.js');
     }
 
