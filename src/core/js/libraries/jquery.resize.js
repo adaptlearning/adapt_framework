03f2e6f5e4a99ef2f38905e790c30618af5af919
Oliver Foster <oliver.foster@kineo.com>
2017-10-05 13:09:17 +0100

    issue/1576: updated to latest jquery.resize
    issue with not stopping loop when no user interaction has occurred.

--- src/core/js/libraries/jquery.resize.js
+++ src/core/js/libraries/jquery.resize.js
@@ -1,5 +1,5 @@
 'use strict';
-// jquery.resize 2017-09-07 https://github.com/adaptlearning/jquery.resize
+// jquery.resize 2017-10-05 https://github.com/adaptlearning/jquery.resize
 
 (function() {
 
@@ -166,6 +166,11 @@
             }
 
             loop.lastMain = (new Date()).getTime();
+            
+            if (this.hasExpired()) {
+                loop.stop();
+                return;
+            }
 
             if (handlers.registered.length == 0) {
                 // nothing to check
