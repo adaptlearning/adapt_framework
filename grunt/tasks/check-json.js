56c297a27bd63cc7c61aabd3a658c3bead909e6d
Oliver Foster <oliver.foster@kineo.com>
2017-10-06 11:09:54 +0100

    issue/1782: grunt check-json respects outputdir config

--- grunt/tasks/check-json.js
+++ grunt/tasks/check-json.js
@@ -5,12 +5,15 @@ module.exports = function(grunt) {
 
           var _ = require('underscore');
           var chalk = require('chalk'); // for some nice colouring
+          var path = require("path");
 
           var listOfCourseFiles = ['course', 'contentObjects', 'articles', 'blocks', 'components'];
           var listOfObjectTypes = ['course', 'menu', 'page', 'article', 'block', 'component' ];
 
+          var sourcedir = grunt.option("outputdir") || grunt.config('sourcedir');
+
           // Go through each course folder inside the <%= sourcedir %>course directory
-          grunt.file.expand({filter: 'isDirectory'}, grunt.config('sourcedir') + 'course/*').forEach(function(path) {
+          grunt.file.expand({filter: 'isDirectory'}, path.join(sourcedir, 'course/*')).forEach(function(path) {
 
               var courseItemObjects = [];
 
