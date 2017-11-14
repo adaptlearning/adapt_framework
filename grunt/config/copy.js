adaptlearning/adapt_framework

commit aa1cc4e18c710ced750f201b94d29c12058e6f40

* issue/1783: prevent flatten of fonts/ libraries/ required/ assets/

grunt/config/copy.js
@@ -2,17 +2,24 @@ module.exports = function (grunt, options) {
     
     var _ = require("underscore");
 
-    var renameAssets = function (destFolder, srcFileName) {
-        var collateAtName = "assets";
-        var collateAtFolder = collateAtName + "/";
-        var startOfCollatePath = srcFileName.indexOf(collateAtFolder) + collateAtFolder.length;
-        var collatedFilePath = destFolder + srcFileName.substr(startOfCollatePath);
-        //ignore the folder alone
-        var testEndsWithCollateName = new RegExp("((?:\\\\|\/)" + collateAtName + ")(?:$|\\\\$|\\\/$)");
-        if (testEndsWithCollateName.test(srcFileName)) {
-            //we have path ending with .../[name] or .../[name]/ discard it
+    var getUnixPath = function(filepath) {
+        // convert to unix style slashes
+        return filepath.replace(/\\/g,"/");
+    };
+    
+    var collate = function(collateAtFolderName, destFolder, srcFileName) {
+        destFolder = getUnixPath(destFolder);
+        srcFileName = getUnixPath(srcFileName);
+
+        // ignore if the srcFileName ends with the collateAtFolderName
+        var nameParts = srcFileName.split("/");
+        if (nameParts[nameParts.length-1] === collateAtFolderName) {
             return destFolder;
         }
+
+        var startOfCollatePath = srcFileName.indexOf(collateAtFolderName) + collateAtFolderName.length + 1;
+        var collatedFilePath = destFolder + srcFileName.substr(startOfCollatePath);
+
         return collatedFilePath;
     }
     
@@ -58,8 +65,7 @@ module.exports = function (grunt, options) {
                     expand: true,
                     src: ['<%= sourcedir %>core/assets/**'],
                     dest: '<%= outputdir %>adapt/css/assets/',
-                    filter: 'isFile',
-                    flatten: true
+                    rename: _.partial(collate, "assets")
                 }
             ]
         },
@@ -72,7 +78,7 @@ module.exports = function (grunt, options) {
                     filter: function(filepath) {
                         return grunt.config('helpers').includedFilter(filepath);
                     },
-                    rename: renameAssets
+                    rename: _.partial(collate, "assets")
                 }
             ]
         },
@@ -85,7 +91,7 @@ module.exports = function (grunt, options) {
                     filter: function(filepath) {
                         return grunt.config('helpers').includedFilter(filepath);
                     },
-                    flatten: true
+                    rename: _.partial(collate, "fonts")
                 }
             ]
         },
@@ -98,7 +104,7 @@ module.exports = function (grunt, options) {
                     filter: function(filepath) {
                         return grunt.config('helpers').includedFilter(filepath);
                     },
-                    rename: renameAssets
+                    rename: _.partial(collate, "assets")
                 }
             ]
         },
@@ -111,7 +117,7 @@ module.exports = function (grunt, options) {
                     filter: function(filepath) {
                         return grunt.config('helpers').includedFilter(filepath);
                     },
-                    flatten: true
+                    rename: _.partial(collate, "fonts")
                 }
             ]
         },
@@ -124,7 +130,7 @@ module.exports = function (grunt, options) {
                     filter: function(filepath) {
                         return grunt.config('helpers').includedFilter(filepath);
                     },
-                    rename: renameAssets
+                    rename: _.partial(collate, "assets")
                 }
             ]
         },
@@ -135,7 +141,7 @@ module.exports = function (grunt, options) {
                     src: ['<%= sourcedir %>core/fonts/**'],
                     dest: '<%= outputdir %>adapt/css/fonts/',
                     filter: 'isFile',
-                    flatten: true
+                    rename: _.partial(collate, "fonts")
                 }
             ]
         },
@@ -148,7 +154,7 @@ module.exports = function (grunt, options) {
                     filter: function(filepath) {
                         return grunt.config('helpers').includedFilter(filepath);
                     },
-                    flatten: true
+                    rename: _.partial(collate, "fonts")
                 }
             ]
         },
@@ -161,7 +167,7 @@ module.exports = function (grunt, options) {
                     filter: function(filepath) {
                         return grunt.config('helpers').includedFilter(filepath);
                     },
-                    flatten: true
+                    rename: _.partial(collate, "assets")
                 }
             ]
         },
@@ -174,7 +180,7 @@ module.exports = function (grunt, options) {
                     filter: function(filepath) {
                         return grunt.config('helpers').includedFilter(filepath);
                     },
-                    flatten: true
+                    rename: _.partial(collate, "fonts")
                 }
             ]
         },
@@ -189,46 +195,27 @@ module.exports = function (grunt, options) {
                 }
             ]
         },
-        libraries: {
-            files: [
-                {
-                    expand: true,
-                    src: [
-                        '<%= sourcedir %>core/js/libraries/*.js'
-                    ],
-                    dest: '<%= outputdir %>libraries/',
-                    filter: 'isFile',
-                    flatten: true
-                }
-            ]
-        },
         required: {
             files: [
                 {
                     expand: true,
-                    src: ['components/**/libraries/**/*', 'extensions/**/libraries/**/*', 'menu/<%= menu %>/libraries/**/*', 'theme/<%= theme %>/libraries/**/*'],
+                    src: ['core/**/libraries/**/*', 'components/**/libraries/**/*', 'extensions/**/libraries/**/*', 'menu/<%= menu %>/libraries/**/*', 'theme/<%= theme %>/libraries/**/*'],
                     cwd: '<%= sourcedir %>',
                     dest: '<%= outputdir %>/libraries/',
                     filter: function(filepath) {
                         return grunt.config('helpers').includedFilter(filepath);
                     },
-                    rename: function(destFolder, srcFileName) {
-                        var endOfRequired = srcFileName.indexOf("libraries/") + 9;
-                        return destFolder + srcFileName.substr(endOfRequired);
-                    }
+                    rename: _.partial(collate, "libraries")
                 },
                 {
                     expand: true,
-                    src: ['components/**/required/**/*', 'extensions/**/required/**/*', 'menu/<%= menu %>/required/**/*', 'theme/<%= theme %>/required/**/*'],
+                    src: ['core/**/required/**/*', 'components/**/required/**/*', 'extensions/**/required/**/*', 'menu/<%= menu %>/required/**/*', 'theme/<%= theme %>/required/**/*'],
                     cwd: '<%= sourcedir %>',
                     dest: '<%= outputdir %>',
                     filter: function(filepath) {
                         return grunt.config('helpers').includedFilter(filepath);
                     },
-                    rename: function(destFolder, srcFileName) {
-                        var endOfRequired = srcFileName.indexOf("required/") + 9;
-                        return destFolder + srcFileName.substr(endOfRequired);
-                    }
+                    rename: _.partial(collate, "required")
                 }
             ]
         }

commit 46cdd13e500657373e00bb1a1fc1ee59a493875f

Resolves #1868, reverts Grunt copy of core 'libraries'
An issue was introduced in aa1cc4e which passed the core 'libraries' folder through the plugin filter, which would prevent the core libraries copying to the build folder for AT users.

grunt/config/copy.js
@@ -195,11 +195,23 @@ module.exports = function (grunt, options) {
                 }
             ]
         },
+        libraries: {
+            files: [
+                {
+                    expand: true,
+                    src: [
+                        '<%= sourcedir %>core/js/libraries/**/*'
+                    ],
+                    dest: '<%= outputdir %>libraries/',
+                    flatten: false
+                }
+            ]
+        },
         required: {
             files: [
                 {
                     expand: true,
-                    src: ['core/**/libraries/**/*', 'components/**/libraries/**/*', 'extensions/**/libraries/**/*', 'menu/<%= menu %>/libraries/**/*', 'theme/<%= theme %>/libraries/**/*'],
+                    src: ['components/**/libraries/**/*', 'extensions/**/libraries/**/*', 'menu/<%= menu %>/libraries/**/*', 'theme/<%= theme %>/libraries/**/*'],
                     cwd: '<%= sourcedir %>',
                     dest: '<%= outputdir %>/libraries/',
                     filter: function(filepath) {