cf8962b657eebcb9815e2a28ab00f70f4b3d669a
Nayan Khedkar <nayan.khedkar@exultcorp.com>
2018-01-08 16:48:13 +0530

    change _isInteractionComplete to _isComplete

--- src/core/js/views/componentView.js
+++ src/core/js/views/componentView.js
@@ -14,7 +14,7 @@ define([
             " " + this.setHidden() +
             " component-" + this.model.get('_layout') + 
             " nth-child-" + this.model.get("_nthChild") +
-            " " + (this.model.get('_isInteractionComplete') ? 'completed' : '');
+            " " + (this.model.get('_isComplete') ? 'completed' : '');
         },
 
         initialize: function(){
