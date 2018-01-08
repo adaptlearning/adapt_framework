bc1888c6c1ed648fdce4a4c0429cd350de19ee5a
Nayan Khedkar <nayan.khedkar@exultcorp.com>
2018-01-08 16:46:17 +0530

    change _isInteractionComplete to _isComplete

--- src/core/js/views/articleView.js
+++ src/core/js/views/articleView.js
@@ -13,7 +13,7 @@ define([
             " " + this.setHidden() +
             " nth-child-" +
             this.model.get("_nthChild") +
-            " " + (this.model.get('_isInteractionComplete') ? 'completed' : '');
+            " " + (this.model.get('_isComplete') ? 'completed' : '');
         }
 
     }, {
