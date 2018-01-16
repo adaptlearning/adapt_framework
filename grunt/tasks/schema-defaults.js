23e8707ce572b96b7086663b014cc69c0c89706e
Nayan Khedkar <nayan.khedkar@exultcorp.com>
2018-01-16 18:32:18 +0530

    issue/1916
    Incorrect course data is loaded when language picker is used

--- grunt/tasks/schema-defaults.js
+++ grunt/tasks/schema-defaults.js
@@ -64,7 +64,7 @@ module.exports = function(grunt) {
             var currentCourseJsonFile = currentCourseFolder + '/' + 'course.' + jsonext;
 
             //read course json and overlay onto defaults object
-            var currentCourseJson = _.deepExtend(defaultsObject, grunt.file.readJSON(currentCourseJsonFile));
+            var currentCourseJson = _.deepExtend({},defaultsObject, grunt.file.readJSON(currentCourseJsonFile));
 
             //write modified course json to build
             grunt.file.write(currentCourseJsonFile, JSON.stringify(currentCourseJson, null, 4));
