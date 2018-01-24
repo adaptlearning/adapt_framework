d5dc91318fad2a851c1e5434e0074cf3056fb6f4
Oliver Foster <oliver.foster@kineo.com>
2018-01-24 15:20:42 +0000

    issue/1513: use a11y_aria_label/a11y_wrap_focus helpers where applicable
    * div/span elements now use a11y_aria_label helper where applicable
    
    * Fixed issue with aria label helpers
    
    * Popups now use a11y_wrap_focus helper plus some other accessibility fixes
    
    * issue/1513: simplified event handler
    
    * issue/1513: normalized indentation

--- src/core/js/accessibility.js
+++ src/core/js/accessibility.js
@@ -117,25 +117,32 @@ define([
             });
 
             Handlebars.registerHelper('a11y_aria_label', function(text) {
-                return '<div class="aria-label prevent-default" tabindex="0" role="region">'+text+'</div>';
+                return '<div class="aria-label prevent-default'+getIgnoreClass()+'" '+getTabIndex()+' role="region">'+text+'</div>';
             });
 
             Handlebars.registerHelper('a11y_aria_label_relative', function(text) {
-                return '<div class="aria-label relative prevent-default" tabindex="0" role="region">'+text+'</div>';
+                return '<div class="aria-label relative prevent-default'+getIgnoreClass()+'" '+getTabIndex()+' role="region">'+text+'</div>';
             });
 
             Handlebars.registerHelper('a11y_wrap_focus', function(text) {
-                return '<a id="a11y-focusguard" class="a11y-ignore a11y-ignore-focus" tabindex="0" role="button">&nbsp;</a>';
+                return '<a class="a11y-focusguard a11y-ignore a11y-ignore-focus" '+getTabIndex()+' role="button">&nbsp;</a>';
             });
 
             Handlebars.registerHelper('a11y_attrs_heading', function(level) {
-                return ' role="heading" aria-level="'+level+'" tabindex="0" ';
+                return ' role="heading" aria-level="'+level+'" '+getTabIndex()+' ';
             });
 
             Handlebars.registerHelper('a11y_attrs_tabbable', function() {
-                return ' role="region" tabindex="0" ';
+                return ' role="region" '+getTabIndex()+' ';
             });
 
+            var getTabIndex = function() {
+                return this.isActive() ? 'tabindex="0"' : 'tabindex="-1"';
+            }.bind(this);
+
+            var getIgnoreClass = function() {
+                return $.a11y.options.isTabbableTextEnabled ? '' : ' a11y-ignore';
+            }.bind(this);
         },
 
         setupToggleButton: function() {
