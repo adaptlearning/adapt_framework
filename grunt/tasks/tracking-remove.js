d36733515d19adf2d2c2a795d88dea549ec3e767
Oliver Foster <oliver.foster@kineo.com>
2018-03-13 12:42:42 +0000

    issue/2006: tracking tasks respect outputdir (#2007)

--- grunt/tasks/tracking-remove.js
+++ grunt/tasks/tracking-remove.js
@@ -1,5 +1,6 @@
 module.exports = function(grunt) {
     var Helpers = require('../helpers')(grunt);
+    var path = require('path');
 
     grunt.registerTask('tracking-remove', 'Removes all tracking IDs', function() {
         if(!Helpers.isPluginInstalled('adapt-contrib-spoor')) return;
@@ -8,7 +9,7 @@ module.exports = function(grunt) {
         function removeTrackingIds(blocksPath, coursePath){
             var blocks = grunt.file.readJSON(blocksPath);
             var course = grunt.file.readJSON(coursePath);
-            
+
             for(var i = 0; i < blocks.length; i++) {
                 delete blocks[i]._trackingId;
             }
@@ -17,13 +18,16 @@ module.exports = function(grunt) {
             grunt.file.write(coursePath, JSON.stringify(course, null, 4));
             grunt.file.write(blocksPath, JSON.stringify(blocks, null, 4));
         }
-        
-        var blocksFiles = grunt.file.expand(options.blocksFile);
-        var courseFiles = grunt.file.expand(options.courseFile);
-        
+
+        var isOutputDir = (grunt.option('outputdir') && grunt.option('outputdir').slice(-5) !== "build");
+        var sourcedir = isOutputDir ? grunt.option('outputdir') : grunt.config('sourcedir');
+
+        var blocksFiles = grunt.file.expand(path.join(sourcedir, options.blocksFile));
+        var courseFiles = grunt.file.expand(path.join(sourcedir, options.courseFile));
+
         for (var i = 0; i < blocksFiles.length; i++) {
             removeTrackingIds(blocksFiles[i], courseFiles[i]);
         }
-        
+
     });
 };
