b25e8ac547b21129951dc6e31cb2848bbd39382d
oliverfoster <oliver.foster@kineo.com>
2018-05-25 15:44:02 +0100

    issue/#2078 - removed accessibilityView and associated code

--- src/core/js/views/navigationView.js
+++ src/core/js/views/navigationView.js
@@ -1,7 +1,6 @@
 define([
-    'core/js/adapt',
-    'core/js/views/accessibilityView'
-], function(Adapt, AccessibilityView) {
+    'core/js/adapt'
+], function(Adapt) {
 
     var NavigationView = Backbone.View.extend({
 
@@ -9,8 +8,7 @@ define([
 
         initialize: function() {
             this.listenToOnce(Adapt, {
-                'courseModel:dataLoading': this.remove,
-                'accessibility:toggle': this.onA11yToggle
+                'courseModel:dataLoading': this.remove
             });
             this.listenTo(Adapt, 'router:menu router:page', this.hideNavigationButton);
             this.template = "navigation";
@@ -39,12 +37,6 @@ define([
                 Adapt.trigger('navigationView:postRender', this);
             }, this));
 
-            this.setupA11yButton();
-
-            if (Adapt.accessibility.isActive()) {
-                this.setupUsageInstructions();
-            }
-
             return this;
         },
 
@@ -64,40 +56,6 @@ define([
 
         showNavigationButton: function() {
             $('.navigation-back-button, .navigation-home-button').removeClass('display-none');
-        },
-
-        setupA11yButton: function() {
-            new AccessibilityView();
-        },
-
-        setupUsageInstructions: function() {
-            var config = Adapt.course.get("_globals")._accessibility;
-
-            if (!config || !config._accessibilityInstructions) {
-                this.$('#accessibility-instructions').remove();
-                return;
-            }
-
-            var instructionsList = config._accessibilityInstructions;
-
-            var usageInstructions;
-            if (instructionsList[Adapt.device.browser]) {
-                usageInstructions = instructionsList[Adapt.device.browser];
-            } else if (Modernizr.touch) {
-                usageInstructions = instructionsList.touch || "";
-            } else {
-                usageInstructions = instructionsList.notouch || "";
-            }
-
-           this.$('#accessibility-instructions').html( usageInstructions );
-        },
-
-        onA11yToggle:function() {
-            // listen once because if a11y active on launch instructions will already be setup
-            
-            if (Adapt.accessibility.isActive()) {
-                this.setupUsageInstructions();
-            }
         }
 
     });
