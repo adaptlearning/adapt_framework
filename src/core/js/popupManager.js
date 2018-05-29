0d0b832cc062b023a766340cca2759a5a4b16d3c
Oliver Foster <oliver.foster@kineo.com>
2018-05-29 11:21:40 +0100

    fixes issue/#2107 : allow defined focus from drawer close

--- src/core/js/popupManager.js
+++ src/core/js/popupManager.js
@@ -4,17 +4,25 @@ define([
 
     Adapt.on('popup:opened', function($element) {
 
-		//capture currently active element or element specified
+        //capture currently active element or element specified
         var $activeElement = $element || $(document.activeElement);
 
         //save tab indexes
         $activeElement.a11y_popup();
     });
 
-    Adapt.on('popup:closed', function() {
+    Adapt.on('popup:closed', function($toElement) {
 
         //restore tab indexes
-        $.a11y_popdown();
+        var $launchedElement = $.a11y_popdown();
+        var $activeElement = $toElement || $launchedElement;
+
+        if ($activeElement) {
+            return $activeElement.focusOrNext();
+        }
+
+        // focus on the first readable element
+        $.a11y_focus();
 
     });
 
