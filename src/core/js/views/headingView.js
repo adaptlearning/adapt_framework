bbc3928169b26874240aeb64ecea0ae5494d23d0
Oliver Foster <oliver.foster@kineo.com>
2018-05-31 09:55:52 +0100

    issue/#2083 - shortened and added custom heading type declaration mechanism

--- src/core/js/views/headingView.js
+++ src/core/js/views/headingView.js
@@ -4,27 +4,24 @@ define([
 
     var HeadingView = Backbone.View.extend({
 
+        attributes: {
+            'aria-live': 'polite',
+            'aria-atomic': 'true'
+        },
+
         initialize: function() {
             this.listenTo(Adapt, "remove", this.remove);
-            this.listenTo(this.model, "change:_isComplete", this.onComplete);
-
-            this.$el.attr({
-                'aria-live': 'polite',
-                'aria-atomic': 'true'
-            });
-
+            this.listenTo(this.model, "change:_isComplete", this.render);
+            this.$el.attr(this.attributes);
             this.render();
-            this.checkCompletion();
         },
 
         render: function() {
             var template = Handlebars.templates[this.constructor.template];
             var data = this.model.toJSON();
+            var customHeadingType = this.$el.attr('data-a11y-heading-type');
+            if (customHeadingType) data._type = customHeadingType;
             this.$el.html(template(data));
-        },
-
-        onComplete: function() {
-            this.render();
             this.checkCompletion();
         },
 
