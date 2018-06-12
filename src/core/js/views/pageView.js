b4d01937c93d95a5a536f129d436481dd4ff1365
Oliver Foster <oliver.foster@kineo.com>
2018-06-12 10:45:06 +0100

    issue/#2126 removed page end aria label

--- src/core/js/views/pageView.js
+++ src/core/js/views/pageView.js
@@ -5,11 +5,11 @@ define([
 ], function(Adapt, AdaptView, ArticleView) {
 
     var PageView = AdaptView.extend({
-        
+
         className: function() {
-            return "page " + 
-            this.model.get('_id') + 
-            " " + this.model.get('_classes') + 
+            return "page " +
+            this.model.get('_id') +
+            " " + this.model.get('_classes') +
             " " + this.setVisibility() +
             " " + (this.model.get('_isComplete') ? 'completed' : '');
         },
@@ -18,18 +18,6 @@ define([
             this.disableAnimation = Adapt.config.has('_disableAnimation') ? Adapt.config.get('_disableAnimation') : false;
             this.$el.css('opacity', 0);
             this.listenTo(this.model, 'change:_isReady', this.isReady);
-
-            var accessibility = Adapt.config.get('_accessibility');
-            if (!accessibility._isEnabled && !accessibility._isEnabledOnTouchDevices) {
-                return;
-            }
-            // create aria-label outside of #wrapper
-            this.$pageLabel = $('<div/>', {
-                'class': 'aria-label relative a11y-ignore-focus prevent-default',
-                tabindex: 0,
-                role: 'region',
-                text: Adapt.course.get('_globals')._accessibility._ariaLabels.pageEnd
-            }).appendTo('body');
         },
 
         isReady: function() {
