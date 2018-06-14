5942e80f85eb0eb70144f2c0e3b5806d6500b411
oliverfoster <oliver.foster@kineo.com>
2018-06-14 11:03:37 +0100

    issue/#2130 Added Array.prototype.find polyfill for IE11

--- src/core/js/libraries/jquery.a11y.js
+++ src/core/js/libraries/jquery.a11y.js
@@ -1,6 +1,27 @@
-//https://github.com/adaptlearning/jquery.a11y 2015-08-13
-
 (function($, window) {
+
+    // POLYFILL Array.prototype.find FOR IE11
+    // https://tc39.github.io/ecma262/#sec-array.prototype.find
+    if (!Array.prototype.find) {
+        Object.defineProperty(Array.prototype, 'find', {
+            value: function(predicate) {
+                if (this == null) throw new TypeError('"this" is null or not defined');
+                var o = Object(this);
+                var len = o.length >>> 0;
+                if (typeof predicate !== 'function') throw new TypeError('predicate must be a function');
+                var thisArg = arguments[1];
+                var k = 0;
+                while (k < len) {
+                    var kValue = o[k];
+                    if (predicate.call(thisArg, kValue, k, o)) return kValue;
+                    k++;
+                }
+                return undefined;
+            },
+            configurable: true,
+            writable: true
+        });
+    }
 
     var iOS = /iPad|iPhone|iPod/.test(navigator.platform);
 
