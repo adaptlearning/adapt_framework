585f21d7eec045af904fa19eb061926a287aa1dc
Oliver Foster <oliver.foster@kineo.com>
2018-06-28 16:47:29 +0100

    accessibility: issue/#2083 added completion status to headers (#2112)

---
+++ src/core/js/headings.js
@@ -0,0 +1,30 @@
+define([
+    './adapt',
+    './views/headingView'
+], function(Adapt, HeadingView) {
+
+    var Headings = Backbone.Controller.extend({
+
+        initialize: function() {
+            var types = [ 'menu', 'page', 'article', 'block', 'component' ];
+            var eventNames = types.concat(['']).join('View:render ');
+            this.listenTo(Adapt, eventNames, this.onViewRender);
+        },
+
+        onViewRender: function(view) {
+            var $headingSeats = view.$('.js-heading');
+            $headingSeats.each(function(index, el) {
+                new HeadingView({
+                    el: el,
+                    model: view.model
+                });
+            });
+        }
+
+    });
+
+    return new Headings();
+
+});
+
+
