fc231429d7455776099d1d248e2dc9d09db8f9e5
moloko <mleathes+github@gmail.com>
2018-10-18 18:13:45 +0100

    remove unused Adapt references

--- src/core/js/models/blockModel.js
+++ src/core/js/models/blockModel.js
@@ -1,13 +1,12 @@
 define([
-    'core/js/adapt',
     'core/js/models/adaptModel'
-], function (Adapt, AdaptModel) {
-	
+], function (AdaptModel) {
+
     var BlockModel = AdaptModel.extend({
         _parent:'articles',
     	_siblings:'blocks',
         _children: 'components',
-        
+
         defaults: function() {
             return _.extend({
                 _sortComponents: true
