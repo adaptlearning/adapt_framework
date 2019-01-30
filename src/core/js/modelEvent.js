fc951a3c8d72b5713e973f32eb85ff2ad7d10549
Oliver Foster <oliver.foster@kineo.com>
2019-01-30 14:34:18 +0000

    converted to space 4

--- src/core/js/modelEvent.js
+++ src/core/js/modelEvent.js
@@ -1,32 +1,32 @@
 define(function() {
 
-  var ModelEvent = Backbone.Controller.extend({
+    var ModelEvent = Backbone.Controller.extend({
 
-    type: null,
-    target: null,
-    value: null,
-    bubbles: true,
-    deepPath: null,
-    target: null,
-    timeStamp: null,
+        type: null,
+        target: null,
+        value: null,
+        bubbles: true,
+        deepPath: null,
+        target: null,
+        timeStamp: null,
 
-    initialize: function(type, target, value) {
-      this.type = type;
-      this.target = target;
-      this.value = value;
-      this.deepPath = [target];
-    },
+        initialize: function(type, target, value) {
+            this.type = type;
+            this.target = target;
+            this.value = value;
+            this.deepPath = [target];
+        },
 
-    stopPropagation: function() {
-      this.bubbles = false;
-    },
+        stopPropagation: function() {
+            this.bubbles = false;
+        },
 
-    addPath: function(target) {
-      this.deepPath.unshift(target);
-    }
+        addPath: function(target) {
+            this.deepPath.unshift(target);
+        }
 
-  });
+    });
 
-  return ModelEvent;
+    return ModelEvent;
 
 });
