49a0d2dacd861fa62b44f6f579c4de6acf546c40
Oliver Foster <oliver.foster@kineo.com>
2019-03-22 16:12:26 +0000

    issue/2370: Do not wrap blocks with only tabbable elements (#2394)

--- src/core/js/libraries/jquery.a11y.js
+++ src/core/js/libraries/jquery.a11y.js
@@ -228,6 +228,7 @@
                 //SEARCH FOR TEXT ONLY NODES AND MAKE TABBABLE
                 var newChildren = [];
                 var newCluster = [];
+                var nonTabbableChildren = 0;
                 for (var i = 0; i < $element[0].childNodes.length; i++) {
                     var child = $element[0].childNodes[i];
                     var cloneChild = $(child.outerHTML)[0];
@@ -236,6 +237,7 @@
                         //IF TEXT NODE WRAP IN A TABBABLE SPAN
                         if (!stringTrim(child.textContent)) break;
                         newCluster.push( child.textContent );
+                        nonTabbableChildren++;
                         break;
                     case 1: //DOM NODE
                         var $child = $(cloneChild);
@@ -252,24 +254,28 @@
                                 //DESCEND INTO NODES WITH CHILDREN
                                 makeChildNodesAccessible($child);
                             }
-                            if (newCluster.length) {
-                                newChildren.push(makeElementTabbable($("<span>"+newCluster.join("")+"</span>")))
-                                newCluster.length = 0;
-                            }
+                            if (newCluster.length) convertClusterToChild();
                             newChildren.push( $child );
                         }
                         break;
                     }
                 }
-                if (newCluster.length) {
-                    newChildren.push(makeElementTabbable($("<span>"+newCluster.join("")+"</span>")))
-                    newCluster.length = 0;
-                }
+                if (newCluster.length) convertClusterToChild();
 
                 removeChildNodes($element);
                 $element.append(newChildren);
 
                 return $element;
+
+                function convertClusterToChild() {
+                    if (!nonTabbableChildren) {
+                        newChildren.push($(newCluster.join("")));
+                    } else {
+                        newChildren.push(makeElementTabbable($("<span>"+newCluster.join("")+"</span>")));
+                    }
+                    newCluster.length = 0;
+                    nonTabbableChildren = 0;
+                }
 
                 function removeChildNodes($element) {
                     var childNodes = $element[0].childNodes.length;
@@ -368,7 +374,7 @@
 
             var scrollToPosition = elementTop - topOffset - (windowAvailableHeight / 2);
             if (scrollToPosition < 0) scrollToPosition = 0;
-            
+
             var isElementTopOutOfView = (elementTop < scrollTopWithTopOffset || elementTop > scrollBottomWithTopOffset);
 
             if (!isElementTopOutOfView) return;
@@ -589,7 +595,7 @@
         }
 
         function nativePreventScroll(event) {
-            // Intermediate function to turn the native event object into a jquery event object. 
+            // Intermediate function to turn the native event object into a jquery event object.
             // preventScroll function is expecting a jquery event object.
             return preventScroll($.event.fix(event));
         }
