632a403b67a635691eb8d3976e3bc686ce7ce4eb
Dan Storey <danielstorey@gmail.com>
2019-04-09 11:20:31 +0100

    #2396 tweak AdaptBuild class

--- grunt/api/index.js
+++ grunt/api/index.js
@@ -1,34 +1,36 @@
 const EventEmitter = require('events');
 const bridge = require('./bridge');
 
-const tasks = [
-    'build',
-    'build-config',
-    'check-json',
-    'clean',
-    'copy',
-    'create-json-config',
-    'dev',
-    'diff',
-    'handlebars',
-    'javascript:compile',
-    'javascript:dev',
-    'less:compile',
-    'less:dev',
-    'minify',
-    'replace',
-    'schema-defaults',
-    'scripts:adaptpostbuild',
-    'tracking-insert',
-    'tracking-remove',
-    'tracking-reset',
-    'translate'
-];
-
-class AdaptBuild extends EventEmitter {
+module.exports = class AdaptBuild extends EventEmitter {
     constructor(options = { outputConfig: 'event' }) {
         super();
-        Object.assign(this, options);
+        this.options = options;
+    }
+
+    get tasks() {
+        return [
+            'build',
+            'build-config',
+            'check-json',
+            'clean',
+            'copy',
+            'create-json-config',
+            'dev',
+            'diff',
+            'handlebars',
+            'javascript:compile',
+            'javascript:dev',
+            'less:compile',
+            'less:dev',
+            'minify',
+            'replace',
+            'schema-defaults',
+            'scripts:adaptpostbuild',
+            'tracking-insert',
+            'tracking-remove',
+            'tracking-reset',
+            'translate'
+        ];
     }
 
     async runTask(task) {
@@ -37,11 +39,11 @@ class AdaptBuild extends EventEmitter {
         const outputEvent = `${taskName}:output`;
 
         // Check task is valid
-        if (!tasks.includes(task)) {
+        if (!this.tasks.includes(task)) {
             return console.log(`Task ${task} does not exist`);
         }
 
-        const output = await bridge.run(task, this.outputConfig);
+        const output = await bridge.run(task, this.options.outputConfig);
 
         this.emit(outputEvent, output);
 
@@ -62,6 +64,4 @@ class AdaptBuild extends EventEmitter {
     }
 
 
-}
-
-module.exports = AdaptBuild;
+};
