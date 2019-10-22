f5c041eb81020ac001bbf1f8a8859ab874d0a81c
Oliver foster <oliver.foster@kineo.com>
2019-10-22 11:40:12 +0100

    Revert "issue/2558 Added contentObject ready wait queue and inview ready lock"
    This reverts commit 66573caf1d0b8735845eafd9925373037d4a7de7.

--- src/core/js/views/menuView.js
+++ src/core/js/views/menuView.js
@@ -19,7 +19,6 @@ define([
     	},
 
         preRender: function() {
-            $.inview.lock('menuView');
             this.disableAnimation = Adapt.config.has('_disableAnimation') ? Adapt.config.get('_disableAnimation') : false;
             this.$el.css('opacity', 0);
             this.listenTo(this.model, 'change:_isReady', this.isReady);
@@ -29,33 +28,26 @@ define([
         },
 
         isReady: function() {
-            if (!this.model.get('_isReady')) return;
-
-            var performIsReady = function() {
-                $('.loading').hide();
-                $(window).scrollTop(0);
-                Adapt.trigger('menuView:ready', this);
-                $.inview.unlock('menuView');
-                var styleOptions = { opacity: 1 };
-                if (this.disableAnimation) {
-                    this.$el.css(styleOptions);
-                    $.inview.unlock('menuView');
-                    $.inview();
-                } else {
-                    this.$el.velocity(styleOptions, {
-                        duration: 'fast',
-                        complete: function() {
-                            $.inview();
-                        }
-                    });
-                }
-                $(window).scroll();
-            }.bind(this);
-
-            Adapt.wait.queue(function() {
-                _.defer(performIsReady);
-            });
-
+            if (this.model.get('_isReady')) {
+                _.defer(function() {
+                    $('.loading').hide();
+                    $(window).scrollTop(0);
+                    Adapt.trigger('menuView:ready', this);
+                    var styleOptions = { opacity: 1 };
+                    if (this.disableAnimation) {
+                        this.$el.css(styleOptions);
+                        $.inview();
+                    } else {
+                        this.$el.velocity(styleOptions, {
+                            duration: 'fast',
+                            complete: function() {
+                                $.inview();
+                            }
+                        });
+                    }
+                    $(window).scroll();
+                }.bind(this));
+            }
         }
 
     }, {
--- src/core/js/views/pageView.js
+++ src/core/js/views/pageView.js
@@ -12,7 +12,7 @@ define([
                 "role": "main"
             }, this);
         },
-
+       
         className: function() {
             return "page " +
             this.model.get('_id') +
@@ -22,39 +22,32 @@ define([
         },
 
         preRender: function() {
-            $.inview.lock('pageView');
             this.disableAnimation = Adapt.config.has('_disableAnimation') ? Adapt.config.get('_disableAnimation') : false;
             this.$el.css('opacity', 0);
             this.listenTo(this.model, 'change:_isReady', this.isReady);
         },
 
         isReady: function() {
-            if (!this.model.get('_isReady')) return;
-
-            var performIsReady = function() {
-                $('.loading').hide();
-                $(window).scrollTop(0);
-                Adapt.trigger('pageView:ready', this);
-                $.inview.unlock('pageView');
-                var styleOptions = { opacity: 1 };
-                if (this.disableAnimation) {
-                    this.$el.css(styleOptions);
-                    $.inview();
-                } else {
-                    this.$el.velocity(styleOptions, {
-                        duration: 'fast',
-                        complete: function() {
-                            $.inview();
-                        }
-                    });
-                }
-                $(window).scroll();
-            }.bind(this);
-
-            Adapt.wait.queue(function() {
-                _.defer(performIsReady);
-            });
-
+            if (this.model.get('_isReady')) {
+                _.defer(_.bind(function() {
+                    $('.loading').hide();
+                    $(window).scrollTop(0);
+                    Adapt.trigger('pageView:ready', this);
+                    var styleOptions = { opacity: 1 };
+                    if (this.disableAnimation) {
+                        this.$el.css(styleOptions);
+                        $.inview();
+                    } else {
+                        this.$el.velocity(styleOptions, {
+                            duration: 'fast',
+                            complete: function() {
+                                $.inview();
+                            }
+                        });
+                    }
+                    $(window).scroll();
+                }, this));
+            }
         },
 
         remove: function() {
