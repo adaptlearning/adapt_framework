275911c0f2e2ff5bcee5ce6041f7c5d8950a1d76
Guy Willis <guy.willis@kineo.com>
2020-03-03 14:03:19 +0000

    Removed _sortComponents code

--- src/core/js/models/blockModel.js
+++ src/core/js/models/blockModel.js
@@ -5,13 +5,7 @@ define([
   var BlockModel = AdaptModel.extend({
     _parent: 'articles',
     _siblings: 'blocks',
-    _children: 'components',
-
-    defaults: function() {
-      return _.extend({
-        _sortComponents: true
-      }, AdaptModel.prototype.defaults);
-    }
+    _children: 'components'
   });
 
   return BlockModel;
