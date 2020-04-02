81cacf19472e216307f3b223eef768d3d045a804
Oliver Foster <oliver.foster@kineo.com>
2020-04-02 16:56:25 +0100

    issue/2714 Decoupled menu, page, article, block and component

--- src/core/js/models/courseModel.js
+++ src/core/js/models/courseModel.js
@@ -6,11 +6,21 @@ define([
   class CourseModel extends MenuModel {
 
     get _parent() {
+      Adapt.log.deprecated('courseModel._parent, use courseModel.getParent() instead, parent models are defined by the JSON');
       return null;
     }
 
     get _siblings() {
+      Adapt.log.deprecated('courseModel._siblings, use courseModel.getSiblings() instead, sibling models are defined by the JSON');
       return null;
+    }
+
+    /**
+     * Returns a string of the model type group.
+     * @returns {string}
+     */
+    getTypeGroup() {
+      return 'course';
     }
 
     initialize(attrs, options) {
