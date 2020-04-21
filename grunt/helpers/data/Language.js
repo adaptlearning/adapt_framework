74e2b6cf261f497916d81e32fb133a9214723ed1
Oliver Foster <olivermartinfoster@gmail.com>
2020-04-21 12:46:16 +0100

    Fix for authoring tool assets.json

--- grunt/helpers/data/Language.js
+++ grunt/helpers/data/Language.js
@@ -80,6 +80,10 @@ class Language {
         hasChanged: false
       });
       file.load();
+      if (jsonFileName === 'assets.json'&& typeof file.data === 'object' && !(file.data instanceof Array) && !file.data._type && !file.data._component && !file.data._model ) {
+        // Skipping as file is the Authoring Tool import/export asset manifest
+        return;
+      }
       this.files.push(file);
     });
 
