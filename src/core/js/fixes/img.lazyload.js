c6c340cbc6ffa8716c5dc528a401a20d0437e8ac
Oliver Foster <olivermartinfoster@gmail.com>
2020-05-04 12:06:17 +0100

    Added config switch and fixed comment

--- src/core/js/fixes/img.lazyload.js
+++ src/core/js/fixes/img.lazyload.js
@@ -5,28 +5,36 @@ define([
 
   /**
    * 27 April 2020 https://github.com/adaptlearning/adapt_framework/issues/2734
-   * Chrome on Android 10 defers load events on images when lite mode is enabled
+   * Chrome on Android defers load events on images when lite mode is enabled
    * and as part of a data saving technique.
    *
    * Add a loading="eager" attribute to all template and partial img tags where
    * the loading attribute is missing.
    */
-  const findImgTag = /<img([^>]*)>/gi;
-  const hasLoadingAttr = / loading=/gi;
-  Adapt.on('template:postRender partial:postRender', function(event) {
-    const imgs = event.value.match(findImgTag);
-    if (!imgs) {
-      // No img tags found
-      return;
-    }
-    event.value = imgs.reduce((value, img) => {
-      // Check if the img tag has a loading attribute
-      if (hasLoadingAttr.test(img)) {
-        return value;
-      }
-      // Add loading="eager" by default
-      return value.replace(img, img.replace(findImgTag, '<img loading="eager"$1>'));
-    }, event.value);
+  Adapt.on('app:dataReady', function() {
+    const config = Adapt.config.get('_fixes');
+    if (config && config._imgLazyLoad === false) return;
+    applyImgLoadingFix();
   });
+
+  function applyImgLoadingFix() {
+    const findImgTag = /<img([^>]*)>/gi;
+    const hasLoadingAttr = / loading=/gi;
+    Adapt.on('template:postRender partial:postRender', function(event) {
+      const imgs = event.value.match(findImgTag);
+      if (!imgs) {
+        // No img tags found
+        return;
+      }
+      event.value = imgs.reduce((value, img) => {
+        // Check if the img tag has a loading attribute
+        if (hasLoadingAttr.test(img)) {
+          return value;
+        }
+        // Add loading="eager" by default
+        return value.replace(img, img.replace(findImgTag, '<img loading="eager"$1>'));
+      }, event.value);
+    });
+  }
 
 });
