3cb343cae2863ade427d4f8037ea2e977bfac0d7
Oliver Foster <olivermartinfoster@gmail.com>
2020-07-04 17:12:59 +0100

    issue/2824 Converted app.js and adapt.js to ES6 modules

--- src/core/js/app.js
+++ src/core/js/app.js
@@ -1,33 +1,29 @@
-require([
-  'core/js/adapt',
-  'core/js/templates',
-  'core/js/fixes',
-  'core/js/accessibility',
-  'core/js/data',
-  'core/js/offlineStorage',
-  'core/js/logging',
-  'core/js/tracking',
-  'core/js/device',
-  'core/js/drawer',
-  'core/js/notify',
-  'core/js/router',
-  'core/js/models/lockingModel',
-  'core/js/mpabc',
-  'core/js/helpers',
-  'core/js/scrolling',
-  'core/js/headings',
-  'core/js/navigation',
-  'plugins'
-], function (Adapt) {
+import Adapt from 'core/js/adapt';
+import 'core/js/templates';
+import 'core/js/fixes';
+import 'core/js/accessibility';
+import 'core/js/data';
+import 'core/js/offlineStorage';
+import 'core/js/logging';
+import 'core/js/tracking';
+import 'core/js/device';
+import 'core/js/drawer';
+import 'core/js/notify';
+import 'core/js/router';
+import 'core/js/models/lockingModel';
+import 'core/js/mpabc';
+import 'core/js/helpers';
+import 'core/js/scrolling';
+import 'core/js/headings';
+import 'core/js/navigation';
+import 'plugins';
 
-  $('body').append(Handlebars.templates.loading());
+$('body').append(Handlebars.templates.loading());
 
-  Adapt.data.on('ready', function triggerInit() {
-    Adapt.log.debug('Calling Adapt.init');
+Adapt.data.on('ready', function triggerInit() {
+  Adapt.log.debug('Calling Adapt.init');
 
-    Adapt.init();
+  Adapt.init();
 
-    Adapt.off('adaptCollection:dataLoaded courseModel:dataLoaded');
-  }).init();
-
-});
+  Adapt.off('adaptCollection:dataLoaded courseModel:dataLoaded');
+}).init();
