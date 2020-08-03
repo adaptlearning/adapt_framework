c149e050f6b22296460d069a1158375f2054bf61
Oliver Foster <olivermartinfoster@gmail.com>
2020-08-04 00:14:43 +0100

    issue/2859 Remove unnecessary typegroups

--- src/core/js/models/itemsComponentModel.js
+++ src/core/js/models/itemsComponentModel.js
@@ -32,14 +32,6 @@ define([
       this.set('_userAnswer', booleanArray);
     }
 
-    /**
-     * Returns a string of the model type group.
-     * @returns {string}
-     */
-    getTypeGroup() {
-      return 'itemscomponent';
-    }
-
     setUpItems() {
       // see https://github.com/adaptlearning/adapt_framework/issues/2480
       const items = this.get('_items') || [];
