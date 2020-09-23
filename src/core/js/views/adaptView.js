9dae09da819865453bd09e4121d102db330416ba
Oliver Foster <olivermartinfoster@gmail.com>
2020-09-23 17:04:38 +0100

    issue/2824-jsx Added linting directive for ReactDOM global

--- src/core/js/views/adaptView.js
+++ src/core/js/views/adaptView.js
@@ -82,6 +82,7 @@ class AdaptView extends Backbone.View {
       return;
     }
     const element = render(this.constructor.template.replace('.jsx', ''), this.model, this);
+    // eslint-disable-next-line no-undef
     ReactDOM.render(element, this.el);
   }
 
@@ -316,6 +317,7 @@ class AdaptView extends Backbone.View {
 
     Adapt.wait.for(end => {
       if (this.isReact) {
+        // eslint-disable-next-line no-undef
         ReactDOM.unmountComponentAtNode(this.el);
       }
       this.$el.off('onscreen.adaptView');
