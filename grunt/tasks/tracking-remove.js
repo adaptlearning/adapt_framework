32c9a46ab0c1e29a95aee24f61c4da4f33d51e9f
moloko <mleathes+github@gmail.com>
2021-03-11 16:24:57 +0000

    fixes #3068

--- grunt/tasks/tracking-remove.js
+++ grunt/tasks/tracking-remove.js
@@ -1,7 +1,6 @@
 module.exports = function(grunt) {
   const Helpers = require('../helpers')(grunt);
   grunt.registerTask('tracking-remove', 'Removes all tracking IDs', function() {
-    if (!Helpers.isPluginInstalled('adapt-contrib-spoor')) return;
     const data = Helpers.getFramework().getData();
     data.removeTrackingIds();
   });
