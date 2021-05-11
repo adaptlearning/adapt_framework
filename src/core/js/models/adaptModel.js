cdfe757d73a35f0a8a7a5ba648b8f38d1cfe9dce
Oliver Foster <oliver.foster@kineo.com>
2021-05-11 16:26:14 +0100

    issue/1486 Ignore optional models in locking

--- src/core/js/models/adaptModel.js
+++ src/core/js/models/adaptModel.js
@@ -764,7 +764,7 @@ export default class AdaptModel extends LockingModel {
       try {
         const model = Adapt.findById(id);
 
-        if (!model.get('_isAvailable')) continue;
+        if (!model.get('_isAvailable') || model.get('_isOptional')) continue;
         if (!model.get('_isComplete')) return true;
       } catch (e) {
         console.warn(`AdaptModel.shouldLock: unknown _lockedBy ID '${id}' found on ${child.get('_id')}`);
