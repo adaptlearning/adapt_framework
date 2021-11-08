7240cd47b7692fbed5d5a2e6bd300c235edbeee8
Oliver Foster <oliver.foster@kineo.com>
2021-11-08 11:27:18 +0000

    issue/3256 Typo

--- grunt/helpers.js
+++ grunt/helpers.js
@@ -219,7 +219,7 @@ module.exports = function(grunt) {
         process.exit();
       }
 
-      const isDevelopmentBuild = process.argv.some(arg => (arg.includes('dev') || arg.includes(':dev')));
+      const isDevelopmentBuild = process.argv.some(arg => (arg === 'dev' || arg.includes(':dev')));
 
       var data = {
         type: isDevelopmentBuild ? 'development' : 'production',
