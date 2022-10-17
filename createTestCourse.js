25fba4c7dd3f6c7aef957222752101eea19d3a15
cahirodoherty-learningpool <cahir.odoherty@learningpool.com>
2022-10-17 15:30:58 +0100

    adapt-cli devDependency and ran lint

--- createTestCourse.js
+++ createTestCourse.js
@@ -4,19 +4,15 @@ const runTest = async () => {
   const type = 'course';
   const contentName = 'test-course';
   const framework = 'v5.22.5';
-  let bypassPrompt = true;
-
-  if (!type || !contentName || !framework) {
-    bypassPrompt = false;
-  }
+  const bypassPrompt = true;
 
   const buildString = `adapt create ${type} '${contentName}' ${framework} ${bypassPrompt}`;
   console.log(`Running command "${buildString}"`);
 
-  await execSync(buildString, {stdio:[0,1,2]});
+  await execSync(buildString, { stdio: [0, 1, 2] });
 
   const serveString = `cd ${contentName} && grunt build && grunt server`;
-  return await execSync(serveString, {stdio:[0,1,2]});
-}
+  return await execSync(serveString, { stdio: [0, 1, 2] });
+};
 
-return runTest();
+runTest();
