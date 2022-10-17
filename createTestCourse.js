0fed3365bf9d783a08db7e9127825ef4df11bf0b
cahirodoherty-learningpool <cahir.odoherty@learningpool.com>
2022-10-17 13:13:56 +0100

    Update: Introduced testing capabilities to the framework

---
+++ createTestCourse.js
@@ -0,0 +1,22 @@
+const { execSync } = require('child_process');
+
+const runTest = async () => {
+  const type = 'course';
+  const contentName = 'test-course';
+  const framework = 'v5.22.5';
+  let bypassPrompt = true;
+
+  if (!type || !contentName || !framework) {
+    bypassPrompt = false;
+  }
+
+  const buildString = `adapt create ${type} '${contentName}' ${framework} ${bypassPrompt}`;
+  console.log(`Running command "${buildString}"`);
+
+  await execSync(buildString, {stdio:[0,1,2]});
+
+  const serveString = `cd ${contentName} && grunt build && grunt server`;
+  return await execSync(serveString, {stdio:[0,1,2]});
+}
+
+return runTest();
