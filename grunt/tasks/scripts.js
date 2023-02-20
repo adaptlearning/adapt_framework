84a08e376971e866b8186e7c2dcc06f59bc3cef2
Oliver Foster <oliver.foster@kineo.com>
2023-02-20 12:31:09 +0000

    Fix: Pass cli variables into scripts (fixes #3365)

--- grunt/tasks/scripts.js
+++ grunt/tasks/scripts.js
@@ -1,21 +1,24 @@
 module.exports = function(grunt) {
 
+  const Helpers = require('../helpers')(grunt);
+  const buildConfig = Helpers.generateConfigData();
+
   grunt.registerTask('scripts', 'Run plugin build tasks', function(mode) {
 
     if (!mode) return;
 
-    var path = require('path');
-    var fs = require('fs');
-    var options = this.options({});
-    var chalk = require('chalk');
-    var async = require('async');
-    var taskCallback = this.async();
+    const path = require('path');
+    const fs = require('fs');
+    const options = this.options({});
+    const chalk = require('chalk');
+    const async = require('async');
+    const taskCallback = this.async();
 
     if (options.plugins) {
 
-      var paths = [];
-      for (var i = 0, l = options.plugins.length; i < l; i++) {
-        var src = options.plugins[i];
+      let paths = [];
+      for (let i = 0, l = options.plugins.length; i < l; i++) {
+        const src = options.plugins[i];
         paths = paths.concat(grunt.file.expand({
           filter: options.pluginsFilter
         }, src));
@@ -24,21 +27,21 @@ module.exports = function(grunt) {
       async.each(paths, function(bowerJSONPath, done) {
 
         if (bowerJSONPath === undefined) return done();
-        var bowerJSON = grunt.file.readJSON(bowerJSONPath);
+        const bowerJSON = grunt.file.readJSON(bowerJSONPath);
 
         if (!bowerJSON.scripts) return done();
 
-        var plugindir = path.dirname(bowerJSONPath);
-        var script = bowerJSON.scripts[mode];
+        const plugindir = path.dirname(bowerJSONPath);
+        const script = bowerJSON.scripts[mode];
 
         if (!script) return done();
 
         try {
-          var buildModule = require(path.join(process.cwd(), plugindir, script));
+          const buildModule = require(path.join(process.cwd(), plugindir, script));
           buildModule(fs, path, grunt.log.writeln, {
-            sourcedir: options.sourcedir,
-            outputdir: options.outputdir,
-            plugindir: plugindir
+            ...buildConfig,
+            ...options,
+            plugindir
           }, done);
         } catch (err) {
           grunt.log.writeln(chalk.red(err));
