e3743b438a8df95e38260e23f03d9f8dcdb36a7e
Eleanor Heath <eleanor.heath@learningpoool.com>
2023-05-19 13:20:21 +0100

    removed react helpers extported templates as is it not used in the jsx template

--- __mocks__/core/js/reactHelpers.js
+++ __mocks__/core/js/reactHelpers.js
@@ -1,12 +1,4 @@
-import React from 'react';
-import Header from '../../../src/core/templates/header';
 import { jest } from '@jest/globals';
-
-export const templates = {
-  header: jest.fn((props) => {
-    return <Header {...props} />;
-  })
-};
 
 export const prefixClasses = jest.fn((classNamePrefixes, classNames) => {
   return classNames.map(className => `${classNamePrefixes}-${className}`).join(' ');
