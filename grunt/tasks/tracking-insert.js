41f7338976727b74888fd3bc4a39869440589d5c
Oliver Foster <oliver.foster@kineo.com>
2023-09-20 14:57:19 +0100

    Fix: Allow tracking ids without spoor installed (fixes #3464)

--- grunt/tasks/tracking-insert.js
+++ grunt/tasks/tracking-insert.js
@@ -1,7 +1,6 @@
 module.exports = function(grunt) {
   const Helpers = require('../helpers')(grunt);
   grunt.registerTask('tracking-insert', 'Adds any missing tracking IDs (starting at the highest existing ID)', function() {
-    if (!Helpers.isPluginInstalled('adapt-contrib-spoor')) return;
     const data = Helpers.getFramework().getData();
     data.addTrackingIds();
   });
