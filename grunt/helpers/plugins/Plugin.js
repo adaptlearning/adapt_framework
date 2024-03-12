9c5f6e7e317b3d6a0490dd307147f119442245b8
Brad Simpson <brad.simpson@kineo.com>
2024-03-12 09:19:51 -0600

    Add semver check to plugins load script

--- grunt/helpers/plugins/Plugin.js
+++ grunt/helpers/plugins/Plugin.js
@@ -64,6 +64,9 @@ class Plugin {
       // assume path name is also plugin name, this shouldn't be necessary
       this.warn(`Plugin folder name ${pathDerivedName} does not match package name ${packageName}.`);
     }
+    if (this.requiredFramework !== undefined && this.isFrameworkCompatible === false) {
+      throw new Error(`Required framework version (${this.requiredFramework}) for plugin ${packageName} not satisfied by current framework version (${this.framework.version}).`);
+    }
     return this;
   }
 
@@ -96,6 +99,16 @@ class Plugin {
   /** @returns {string} */
   get version() {
     return this.packageJSONFile.firstFileItem.item.version;
+  }
+
+  /** @returns {string} */
+  get requiredFramework() {
+    return this.packageJSONFile.firstFileItem.item.framework;
+  }
+
+  /** @returns {boolean} */
+  get isFrameworkCompatible() {
+    return semver.satisfies(this.framework.version, this.requiredFramework);
   }
 
   /**
