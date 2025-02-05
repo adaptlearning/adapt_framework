77454e07e5b9944d3e0f14ebd0d08d8b433d7c12
Oliver Foster <oliver.foster@kineo.com>
2025-02-05 13:51:58 +0000

    Fix: Added timestamp to build.min.js

--- grunt/config/watch.js
+++ grunt/config/watch.js
@@ -2,73 +2,73 @@
 module.exports = {
   bowerJson: {
     files: ['<%= sourcedir %>*/*/bower.json'],
-    tasks: ['dev']
+    tasks: ['build-config', 'dev']
   },
   scripts: {
     files: ['<%= sourcedir %>*/*/scripts/*'],
-    tasks: ['dev']
+    tasks: ['build-config', 'dev']
   },
   less: {
     files: ['<%= sourcedir %>**/*.less'],
-    tasks: ['less:dev']
+    tasks: ['build-config', 'less:dev']
   },
   handlebars: {
     files: ['<%= sourcedir %>**/*.hbs'],
-    tasks: ['handlebars', 'javascript:dev']
+    tasks: ['build-config', 'handlebars', 'javascript:dev']
   },
   courseJson: {
     files: ['<%= sourcedir %><%= coursedir %>/**/*.<%= jsonext %>', '<%= outputdir %><%= coursedir %>/*/language_data_manifest.js'],
-    tasks: ['language-data-manifests', 'jsonlint', 'check-json', 'newer:copy:courseJson', 'schema-defaults']
+    tasks: ['build-config', 'language-data-manifests', 'jsonlint', 'check-json', 'newer:copy:courseJson', 'schema-defaults']
   },
   courseAssets: {
     files: ['<%= sourcedir %><%= coursedir %>/<%=languages%>/*', '!<%= sourcedir %><%= coursedir %>/<%=languages%>/*.<%= jsonext %>'],
-    tasks: ['newer:copy:courseAssets']
+    tasks: ['build-config', 'newer:copy:courseAssets']
   },
   js: {
     files: ['<%= sourcedir %>**/*.js', '<%= sourcedir %>**/*.jsx'],
     options: {
       spawn: false
     },
-    tasks: ['javascript:dev', 'clean:temp']
+    tasks: ['build-config', 'javascript:dev', 'clean:temp']
   },
   componentsAssets: {
     files: ['<%= sourcedir %>components/**/assets/**'],
-    tasks: ['newer:copy:componentAssets']
+    tasks: ['build-config', 'newer:copy:componentAssets']
   },
   componentsFonts: {
     files: ['<%= sourcedir %>components/**/fonts/**'],
-    tasks: ['newer:copy:componentFonts']
+    tasks: ['build-config', 'newer:copy:componentFonts']
   },
   extensionsAssets: {
     files: ['<%= sourcedir %>extensions/**/assets/**'],
-    tasks: ['newer:copy:extensionAssets']
+    tasks: ['build-config', 'newer:copy:extensionAssets']
   },
   extensionsFonts: {
     files: ['<%= sourcedir %>extensions/**/fonts/**'],
-    tasks: ['newer:copy:extensionFonts']
+    tasks: ['build-config', 'newer:copy:extensionFonts']
   },
   menuAssets: {
     files: ['<%= sourcedir %>menu/<%= menu %>/**/assets/**'],
-    tasks: ['newer:copy:menuAssets']
+    tasks: ['build-config', 'newer:copy:menuAssets']
   },
   menuFonts: {
     files: ['<%= sourcedir %>menu/<%= menu %>/**/fonts/**'],
-    tasks: ['newer:copy:menuFonts']
+    tasks: ['build-config', 'newer:copy:menuFonts']
   },
   themeAssets: {
     files: ['<%= sourcedir %>theme/<%= theme %>/**/assets/**'],
-    tasks: ['newer:copy:themeAssets']
+    tasks: ['build-config', 'newer:copy:themeAssets']
   },
   themeFonts: {
     files: ['<%= sourcedir %>theme/<%= theme %>/**/fonts/**'],
-    tasks: ['newer:copy:themeFonts']
+    tasks: ['build-config', 'newer:copy:themeFonts']
   },
   libraries: {
     files: ['<%= sourcedir %>core/libraries/**/*', '<%= sourcedir %>*/*/libraries/**/*'],
-    tasks: ['newer:copy:libraries']
+    tasks: ['build-config', 'newer:copy:libraries']
   },
   required: {
     files: ['<%= sourcedir %>core/required/**/*', '<%= sourcedir %>*/*/required/**/*'],
-    tasks: ['newer:copy:required']
+    tasks: ['build-config', 'newer:copy:required']
   }
 };
